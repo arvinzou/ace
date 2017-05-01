7e3cb524aa0245898740bcba1b8fb5bd1ec33ce4
249134995 <249134995@qq.com>
2017-05-01 15:10:06 +0800

    电装运营会议建立

---
+++ ace/ace-operana/ace-operana-service/src/main/java/com/huacainfo/ace/operana/dao/MeetingTopicDao.java
@@ -0,0 +1,12 @@
+package com.huacainfo.ace.operana.dao;
+
+import com.huacainfo.ace.operana.model.MeetingTopic;
+import org.apache.ibatis.annotations.Param;
+
+public interface MeetingTopicDao {
+    int deleteByMeetingId(String meetingId);
+
+    int insert(MeetingTopic record);
+
+    int updateTopicOwner(@Param("userId")String userId,@Param("meetingId")String meetingId,@Param("topicId")String topicId);
+}
