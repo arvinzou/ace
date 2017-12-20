56a0b4b0cfe0167066073c849b71abfba69fc4dc
249134995 <249134995@qq.com>
2017-12-20 10:11:15 +0800

    websocket框架

---
+++ ace/ace-live/ace-live-web/src/main/java/com/huacainfo/ace/live/web/controller/LiveBaseController.java
@@ -0,0 +1,12 @@
+package com.huacainfo.ace.live.web.controller;
+
+import java.io.Serializable;
+
+import com.huacainfo.ace.common.web.controller.BaseController;
+
+public class LiveBaseController extends BaseController implements
+		Serializable {
+	private static final long serialVersionUID = 1L;
+
+	
+}
