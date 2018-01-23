b91b6e0a57a196d981560880bf9cebb270bbc856
249134995 <249134995@qq.com>
2018-01-23 15:11:18 +0800

    录音功能

--- ace/ace-live/ace-live-web/src/main/webapp/content/www/live/msg.js
+++ ace/ace-live/ace-live-web/src/main/webapp/content/www/live/msg.js
@@ -6,59 +6,7 @@
        };*/
 
 
-      //连接发生错误的回调方法
-      websocketMsg.onerror = function(){
-          var url="ws://"+cfg.websocketurl+"/live/websocket/"+lvsCmd.urlParams.id+"/"+wxuser.openid+"/msg";
-          alert(url);
-          setTimeout("initWebSocket",5000);
-      };
 
-      //连接成功建立的回调方法
-      websocketMsg.onopen = function(event){
-          //setMessageInnerHTML("open");
-         // alert("onopen ok");
-      };
-
-      //接收到消息的回调方法
-      websocketMsg.onmessage = function(){
-          var data=JSON.parse(event.data);
-            console.log(data);
-          if(data.header.type==1){
-             var tpl = document.getElementById('tpl-msg').innerHTML;
-             var html = juicer(tpl, data);
-             $("#chatContent").append(html);
-            // var chatlist = document.getElementById('chatlist');
-             //chatlist.scrollTop = chatlist.scrollHeight;
-          }
-          if(data.header.type==2){
-                console.log(data.header.type);
-               var tpl = document.getElementById('tpl-cmt').innerHTML;
-               var html = juicer(tpl, data);
-               var el="#cmtlist"+data.id;
-               $(el).append(html);
-                $(el).removeClass("fn-hide");
-                var els="#j-remark-"+data.id+" em";
-                $(els).removeClass("fn-hide");
-               //var cmtlist = document.getElementById(el);
-               //cmtlist.scrollTop = cmtlist.scrollHeight;
-           }
-           if(data.header.type==3){
-              load=false;
-              getReport(reportPage);
-          }
-          //alert(data.header.type);
-
-      };
-
-      //连接关闭的回调方法
-      websocketMsg.onclose = function(){
-          //setMessageInnerHTML("close");
-      };
-
-      //监听窗口关闭事件，当窗口关闭时，主动去关闭websocketMsg连接，防止连接还没断开就关闭窗口，server端会抛异常。
-      window.onbeforeunload = function(){
-          websocketMsg.close();
-      };
 
       //将消息显示在网页上
       function setMessageInnerHTML(innerHTML){
@@ -95,11 +43,69 @@
       function initWebSocket(){
       //判断当前浏览器是否支持WebSocket
             if('WebSocket' in window){
-                var url="ws://"+cfg.websocketurl+"/live/websocket/"+lvsCmd.urlParams.id+"/"+wxuser.openid+"/msg";
-                websocketMsg = new ReconnectingWebSocket(url);
+              var url="ws://"+cfg.websocketurl+"/live/websocket/"+lvsCmd.urlParams.id+"/"+wxuser.openid+"/msg";
+              websocketMsg = new ReconnectingWebSocket(url);
+              console.log("start init websocket at");
+              console.log(url);
+              console.log(websocketMsg);
+                //连接发生错误的回调方法
+              websocketMsg.onerror = function(){
+                  var url="ws://"+cfg.websocketurl+"/live/websocket/"+lvsCmd.urlParams.id+"/"+wxuser.openid+"/msg";
+                  alert(url);
+                  console.log("websocket init onerror it will reconnecting after 5000 ms");
+                  setTimeout("initWebSocket()",5000);
+              };
+
+              //连接成功建立的回调方法
+              websocketMsg.onopen = function(event){
+                  //setMessageInnerHTML("open");
+                 // alert("onopen ok");
+              };
+
+              //接收到消息的回调方法
+              websocketMsg.onmessage = function(){
+                  var data=JSON.parse(event.data);
+                    console.log(data);
+                  if(data.header.type==1){
+                     var tpl = document.getElementById('tpl-msg').innerHTML;
+                     var html = juicer(tpl, data);
+                     $("#chatContent").append(html);
+                    // var chatlist = document.getElementById('chatlist');
+                     //chatlist.scrollTop = chatlist.scrollHeight;
+                  }
+                  if(data.header.type==2){
+                        console.log(data.header.type);
+                       var tpl = document.getElementById('tpl-cmt').innerHTML;
+                       var html = juicer(tpl, data);
+                       var el="#cmtlist"+data.id;
+                       $(el).append(html);
+                        $(el).removeClass("fn-hide");
+                        var els="#j-remark-"+data.id+" em";
+                        $(els).removeClass("fn-hide");
+                       //var cmtlist = document.getElementById(el);
+                       //cmtlist.scrollTop = cmtlist.scrollHeight;
+                   }
+                   if(data.header.type==3){
+                      load=false;
+                      getReport(reportPage);
+                  }
+                  //alert(data.header.type);
+
+              };
+
+              //连接关闭的回调方法
+              websocketMsg.onclose = function(){
+                  //setMessageInnerHTML("close");
+              };
+
+
             }
             else{
-                alert('Not support websocketMsg');
+                alert('Not support websocket');
             }
       }
-      setTimeout("initWebSocket",3000);
+       //监听窗口关闭事件，当窗口关闭时，主动去关闭websocketMsg连接，防止连接还没断开就关闭窗口，server端会抛异常。
+    window.onbeforeunload = function(){
+        websocketMsg.close();
+    };
+      setTimeout("initWebSocket()",3000);
