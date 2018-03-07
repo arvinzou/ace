e65639ec08de7f723d1025c07a075e61503e800b
249134995 <249134995@qq.com>
2018-03-07 14:57:03 +0800

    智慧治超

---
+++ ace/ace-woc/ace-woc-service/src/test/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Arvin on 2017/11/20.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+
+    }
+}
---
+++ ace/ace-woc/ace-woc-web/src/main/java/com/huacainfo/ace/face/web/controller/AnalysisController.java
@@ -0,0 +1,31 @@
+package com.huacainfo.ace.face.web.controller;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.huacainfo.ace.common.result.ListResult;
+import com.huacainfo.ace.woc.service.AnalysisService;
+
+@Controller
+@RequestMapping("/anslysis")
+public class AnalysisController extends WocBaseController {
+	private static final long serialVersionUID = 1L;
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private AnalysisService analysisService;
+
+	@RequestMapping(value = "/query.do")
+	@ResponseBody
+	public ListResult<Map<String,Object>> query(
+			 String reportId)
+			throws Exception {
+		Map<String,Object> condition=this.getParams();
+		return analysisService.query(condition, reportId,0,0);
+	}	
+}
