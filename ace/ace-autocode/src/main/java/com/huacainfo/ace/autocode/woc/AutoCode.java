fc065f2e32e34556fafb3d4a64d04cda643488ec
249134995 <249134995@qq.com>
2018-03-07 17:00:20 +0800

    智慧治超

---
+++ ace/ace-autocode/src/main/java/com/huacainfo/ace/autocode/woc/AutoCode.java
@@ -0,0 +1,23 @@
+package com.huacainfo.ace.autocode.woc;
+
+import com.huacainfo.ace.autocode.base.AutoCodeUtils;
+
+public class AutoCode {
+    public static void main(String[] args) throws Exception {
+        AutoCodeUtils autoCodeUtils = new AutoCodeUtils("woc", "图片", "王恩");
+        Class<?> c = com.huacainfo.ace.live.model.Img.class;
+        autoCodeUtils.init(c);
+        autoCodeUtils.createVO(c);
+        autoCodeUtils.createQVO(c);
+        autoCodeUtils.createDAO(c);
+        autoCodeUtils.createService(c);
+        autoCodeUtils.createServiceImpl(c);
+        autoCodeUtils.createAction(c);
+        //autoCodeUtils.createJsp(c);
+        //autoCodeUtils.createJsConfig(c);
+        //autoCodeUtils.createJsView(c);
+        //autoCodeUtils.createJsController(c);
+        autoCodeUtils.createModel(c);
+
+    }
+}
