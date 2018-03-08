7ee6802f0f7020e081627aed54ee92823f76610b
249134995 <249134995@qq.com>
2018-03-08 09:01:19 +0800

    智慧治超

--- ace/ace-woc/ace-woc-rest/src/main/java/com/huacainfo/ace/woc/web/controller/TokenController.java
+++ ace/ace-woc/ace-woc-rest/src/main/java/com/huacainfo/ace/woc/web/controller/TokenController.java
@@ -3,6 +3,7 @@ package com.huacainfo.ace.woc.web.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.huacainfo.ace.common.model.Userinfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +13,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @Controller
-@RequestMapping("/v1")
+@RequestMapping("/v1/token")
 public class TokenController extends WocBaseController {
 	private static final long serialVersionUID = 1L;
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@RequestMapping(value = "/token")
-	public ResponseEntity token(@RequestParam String username, @RequestParam String password,@RequestParam String timestamp)
+	@RequestMapping(value = "/getToken")
+	public ResponseEntity getToken(@RequestParam String username, @RequestParam String password,@RequestParam String timestamp)
 			throws Exception {
 		Map<String,Object> rst=new HashMap<>();
 		rst.put("access_token","2YotnFZFEjr1zCsicMWpAA");
@@ -39,5 +41,22 @@ public class TokenController extends WocBaseController {
 		rst.put("access_token",access_token);
 		rst.put("scope",scope);
 		return new ResponseEntity<>(rst, HttpStatus.OK);
+	}
+	@RequestMapping("/requestJson")
+	@ResponseBody
+	public Userinfo requestJson(@RequestBody Userinfo userinfo){
+		//@ResponseBody将itemsCustom转成json格式输出
+		/*
+		$.ajax({
+				type:'post',
+				url:'${pageContext.request.contextPath }/requestJson.action',
+				contentType:'application/json;charset=utf-8',
+				//数据格式是json串,商品信息
+				data:'{"name":"手机","price":999}',
+				success:function(data){//返回json结果
+			alert(data);
+		}
+            });*/
+		return userinfo;
 	}
 }
