afbd7d0f124bfbdf8614539fea242cda3adca269
249134995@qq.com <249134995@qq.com>
2018-03-09 09:12:52 +0800

    智慧治超数据模型

---
+++ ace/ace-woc/ace-woc-client/src/main/java/com/huacainfo/ace/woc/model/Device.java
@@ -0,0 +1,205 @@
+package com.huacainfo.ace.woc.model;
+
+import java.util.Date;
+
+public class Device {
+    private String id;
+
+    private String deviceName;
+
+    private String deviceNo;
+
+    private String deviceType;
+
+    private String deviceStatus;
+
+    private Date onlineDate;
+
+    private Date offlineDate;
+
+    private String manufacturer;
+
+    private Date manufactureDate;
+
+    private String contactName;
+
+    private String contactPhoneNumber;
+
+    private String ipAddr;
+
+    private String remark;
+
+    private String status;
+
+    private String createUserId;
+
+    private String createUserName;
+
+    private Date createDate;
+
+    private String lastModifyUserId;
+
+    private String lastModifyUserName;
+
+    private Date lastModifyDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName == null ? null : deviceName.trim();
+    }
+
+    public String getDeviceNo() {
+        return deviceNo;
+    }
+
+    public void setDeviceNo(String deviceNo) {
+        this.deviceNo = deviceNo == null ? null : deviceNo.trim();
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType == null ? null : deviceType.trim();
+    }
+
+    public String getDeviceStatus() {
+        return deviceStatus;
+    }
+
+    public void setDeviceStatus(String deviceStatus) {
+        this.deviceStatus = deviceStatus == null ? null : deviceStatus.trim();
+    }
+
+    public Date getOnlineDate() {
+        return onlineDate;
+    }
+
+    public void setOnlineDate(Date onlineDate) {
+        this.onlineDate = onlineDate;
+    }
+
+    public Date getOfflineDate() {
+        return offlineDate;
+    }
+
+    public void setOfflineDate(Date offlineDate) {
+        this.offlineDate = offlineDate;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer == null ? null : manufacturer.trim();
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName == null ? null : contactName.trim();
+    }
+
+    public String getContactPhoneNumber() {
+        return contactPhoneNumber;
+    }
+
+    public void setContactPhoneNumber(String contactPhoneNumber) {
+        this.contactPhoneNumber = contactPhoneNumber == null ? null : contactPhoneNumber.trim();
+    }
+
+    public String getIpAddr() {
+        return ipAddr;
+    }
+
+    public void setIpAddr(String ipAddr) {
+        this.ipAddr = ipAddr == null ? null : ipAddr.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId == null ? null : createUserId.trim();
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName == null ? null : createUserName.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getLastModifyUserId() {
+        return lastModifyUserId;
+    }
+
+    public void setLastModifyUserId(String lastModifyUserId) {
+        this.lastModifyUserId = lastModifyUserId == null ? null : lastModifyUserId.trim();
+    }
+
+    public String getLastModifyUserName() {
+        return lastModifyUserName;
+    }
+
+    public void setLastModifyUserName(String lastModifyUserName) {
+        this.lastModifyUserName = lastModifyUserName == null ? null : lastModifyUserName.trim();
+    }
+
+    public Date getLastModifyDate() {
+        return lastModifyDate;
+    }
+
+    public void setLastModifyDate(Date lastModifyDate) {
+        this.lastModifyDate = lastModifyDate;
+    }
+}
--- ace/ace-woc/ace-woc-web/src/main/java/com/huacainfo/ace/woc/web/controller/WocBaseController.java
+++ ace/ace-woc/ace-woc-web/src/main/java/com/huacainfo/ace/woc/web/controller/WocBaseController.java
@@ -5,8 +5,9 @@ import com.huacainfo.ace.common.web.controller.BaseController;
 import java.io.Serializable;
 
 public class WocBaseController extends BaseController implements
-		Serializable {
-	private static final long serialVersionUID = 1L;
+        Serializable {
+
+    private static final long serialVersionUID = 1L;
 
 	
 }
