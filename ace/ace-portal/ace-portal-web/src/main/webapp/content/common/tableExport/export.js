fecbccbbd8cd1ab60c372f2aca365217d80172d7
249134995 <249134995@qq.com>
2018-03-15 10:16:18 +0800

    智慧治超

--- ace/ace-portal/ace-portal-web/src/main/webapp/content/common/tableExport/export.js
+++ ace/ace-portal/ace-portal-web/src/main/webapp/content/common/tableExport/export.js
@@ -1,18 +1,17 @@
 var TYPE_NAME = {
         json: 'JSON',
         xml: 'XML',
-       // png: 'PNG',
+        png: 'PNG',
         csv: 'CSV',
         txt: 'TXT',
-        sql: 'SQL'
-        //doc: 'MS-Word',
-       // excel: 'MS-Excel',
-       // powerpoint: 'MS-Powerpoint',
-       // pdf: 'PDF'
+        sql: 'SQL',
+        doc: 'MS-Word',
+        excel: 'MS-Excel',
+        powerpoint: 'MS-Powerpoint',
+        pdf: 'PDF'
 };
 var $menu = $('<div class="top-menu"><ul class="nav navbar-nav pull-left"><li class="dropdown dropdown-user"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"><span class="username username-hide-on-mobile">导出</span> <i class="fa fa-angle-down"></i></a><ul class="dropdown-menu dropdown-menu-default"></ul></li></ul></div>');
-//var exportTypes = ['txt','excel','json','xml','png','csv','sql','doc','pdf','powerpoint'];
-var exportTypes = ['txt','json','xml','csv','sql'];
+var exportTypes = ['txt','excel','json','xml','png','csv','sql','doc','pdf','powerpoint'];
 var tempStr = "";
 for(var i = 0;i < exportTypes.length;i++){
     tempStr += '<li data-type="'+exportTypes[i]+'"><a href="javascript:void(0)">'+TYPE_NAME[exportTypes[i]]+'</a></li>';
@@ -26,8 +25,13 @@ $menu.find(".dropdown-menu-default li").click(function () {
             ee.find('.jqgfirstrow').remove();//干掉多余的无效行
             ee.find('tbody').before(dd);//合并表头和表数据
             ee.find('tr.ui-search-toolbar').remove();//干掉搜索框
-            console.log({type:type,escape:'false',fileName: cfg.fileName+'v'+getNowFormatDate()});
-            ee.tableExport({type:type,escape:'false',fileName: cfg.fileName+'v'+getNowFormatDate()});
+            console.log({type:type,fileName: cfg.fileName+'v'+getNowFormatDate()});
+            //Regex reg = new Regex(@"(?i)<(/?(?:table|tr|td))\b[^>]*>");
+            var domstr=$(ee).html();
+            $("#tableExport").html("<table id='exportHandle'><caption>"+cfg.fileName+"</caption>"+domstr.replace(/<(?!(table|tbody|th|thead|tr|td)[ >])[^>/]*>/img,"")+"</table>");
+            $("#tableExport").removeClass("hide");
+            $("#exportHandle").tableExport({type:type,fileName: cfg.fileName+'v'+getNowFormatDate()});
+            $("#tableExport").addClass("hide");
         };
    doExport();
 });
