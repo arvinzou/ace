1c9549462201e722afe88cc6bd6470b05f5725cd
249134995@qq.com <249134995@qq.com>
2018-03-27 09:30:42 +0800

    update

--- ace/ace-autocode/src/main/java/com/huacainfo/ace/autocode/woc/AutoCode.java
+++ ace/ace-autocode/src/main/java/com/huacainfo/ace/autocode/woc/AutoCode.java
@@ -6,21 +6,21 @@ public class AutoCode {
 
     public static void main(String[] args) throws Exception {
 //        AutoCodeUtils autoCodeUtils = new AutoCodeUtils("woc", "道路档案", "Arvin",
-//                "C:\\Users\\Arvin\\IdeaProjects\\ace\\ace");
-        AutoCodeUtils autoCodeUtils = new AutoCodeUtils("woc", "黑名单档案", "Arvin");
-        Class<?> c = com.huacainfo.ace.woc.model.Blacklist.class;
-        autoCodeUtils.init(c);
-        autoCodeUtils.createVO(c);
-        autoCodeUtils.createQVO(c);
-        autoCodeUtils.createDAO(c);
-        autoCodeUtils.createService(c);
-        autoCodeUtils.createServiceImpl(c);
-        autoCodeUtils.createAction(c);
-        autoCodeUtils.createJsp(c);
-        autoCodeUtils.createJsConfig(c);
-        autoCodeUtils.createJsView(c);
-        autoCodeUtils.createJsController(c);
-        autoCodeUtils.createModel(c);
+//                 AutoCodeUtils autoCodeUtils = new AutoCodeUtils("woc", "黑名单档案", "Arvin");
+////        Class<?> c = com.huacainfo.ace.woc.model.Blacklist.class;
+////        autoCodeUtils.init(c);
+////        autoCodeUtils.createVO(c);
+////        autoCodeUtils.createQVO(c);
+////        autoCodeUtils.createDAO(c);
+////        autoCodeUtils.createService(c);
+////        autoCodeUtils.createServiceImpl(c);
+////        autoCodeUtils.createAction(c);
+////        autoCodeUtils.createJsp(c);
+////        autoCodeUtils.createJsConfig(c);
+////        autoCodeUtils.createJsView(c);
+////        autoCodeUtils.createJsController(c);
+////        autoCodeUtils.createModel(c);       "C:\\Users\\Arvin\\IdeaProjects\\ace\\ace");
+//
     }
 
 }
