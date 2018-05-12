3ade2666bf2a4d9ea1223558ec4ac9e0cc3e68f8
249134995@qq.com <249134995@qq.com>
2018-05-12 15:28:17 +0800

    工商联

--- ace/ace-fop/ace-fop-web/src/main/webapp/www/html/information/js/infomation.js
+++ ace/ace-fop/ace-fop-web/src/main/webapp/www/html/information/js/infomation.js
@@ -1,4 +1,5 @@
 var pageSize=10;
+var noticeType = "";
 var ngControllerName = "angularjsCtrl";
 var ngAppName = "angularjsApp";
 //angularjs Controller初始化
@@ -6,11 +7,12 @@ var app =angular.module(ngAppName, []);
 
 app.controller(ngControllerName,function($scope){
     // 初始化查询所有
+    noticeType = $("#noticeType").val();
     $.ajax({
         url: "/fop/www/findNoticeList",
         type:"post",
         async:false,
-        data:{"limit":pageSize, noticeType:"1", page: 1},
+        data:{"limit":pageSize, noticeType:noticeType, page: 1},
         success:function(result){
             if(result.status == 0) {
                 $scope.items = result.data.list;
@@ -35,7 +37,7 @@ app.controller(ngControllerName,function($scope){
             url: "/fop/www/findNoticeList",
             type:"post",
             async:false,
-            data:{"limit":pageSize, noticeType:"1", page: 1, title : key_word},
+            data:{"limit":pageSize, noticeType:noticeType, page: 1, title : key_word},
             success:function(result){
                 if(result.status == 0) {
                     $scope.items = result.data.list;
@@ -71,7 +73,7 @@ app.controller(ngControllerName,function($scope){
             url: "/fop/www/findNoticeList",
             type:"post",
             async:false,
-            data:{"limit":pageSize, noticeType:"1", page: 1, sord : orderParam},
+            data:{"limit":pageSize, noticeType:noticeType, page: 1, sord : orderParam},
             success:function(result){
                 if(result.status == 0) {
                     $scope.items = result.data.list;
