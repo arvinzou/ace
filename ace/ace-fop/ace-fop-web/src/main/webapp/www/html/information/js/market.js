8a6b7b69097b828d3fdae841cb9530cd57e5dca3
249134995@qq.com <249134995@qq.com>
2018-05-12 15:03:23 +0800

    工商联

---
+++ ace/ace-fop/ace-fop-web/src/main/webapp/www/html/information/js/market.js
@@ -0,0 +1,90 @@
+var pageSize=10;
+var ngControllerName = "angularjsCtrl";
+var ngAppName = "angularjsApp";
+//angularjs Controller初始化
+var app =angular.module(ngAppName, []);
+
+app.controller(ngControllerName,function($scope){
+    // 初始化查询所有
+    $.ajax({
+        url: "/fop/www/findNoticeList",
+        type:"post",
+        async:false,
+        data:{"limit":pageSize, noticeType:"1", page: 1},
+        success:function(result){
+            if(result.status == 0) {
+                $scope.items = result.data.list;
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+            }else {
+                alert(result.info);
+            }
+        },
+        error:function(){
+            alert("内部服务异常");
+        }
+    });
+
+    /**
+     * 按关键字搜索
+     */
+    $scope.search = function(){
+        var key_word = $("#key_word").val();
+        $.ajax({
+            url: "/fop/www/findNoticeList",
+            type:"post",
+            async:false,
+            data:{"limit":pageSize, noticeType:"1", page: 1, title : key_word},
+            success:function(result){
+                if(result.status == 0) {
+                    $scope.items = result.data.list;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                }else {
+                    alert(result.info);
+                }
+            },
+            error:function(){
+                alert("内部服务异常");
+            }
+        });
+    }
+
+    /**
+     * 列表排序
+     */
+    $scope.sortList = function(name){
+        var flag = name;
+        var orderParam = "";
+        if(flag == 'asc'){
+            orderParam = 'asc';
+            $("#"+flag).hide();
+            $("#desc").show();
+        }else{
+            orderParam = 'desc';
+            $("#"+flag).hide();
+            $("#asc").show();
+        }
+        $.ajax({
+            url: "/fop/www/findNoticeList",
+            type:"post",
+            async:false,
+            data:{"limit":pageSize, noticeType:"1", page: 1, sord : orderParam},
+            success:function(result){
+                if(result.status == 0) {
+                    $scope.items = result.data.list;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                }else {
+                    alert(result.info);
+                }
+            },
+            error:function(){
+                alert("内部服务异常");
+            }
+        });
+    }
+});
