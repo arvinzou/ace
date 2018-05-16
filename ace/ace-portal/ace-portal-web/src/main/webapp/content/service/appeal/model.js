b7dfd41b453a971f672f6ee0fc23df8edb81facc
249134995 <249134995@qq.com>
2018-05-16 18:47:26 +0800

    修改tpl.xml重复sql

--- ace/ace-portal/ace-portal-web/src/main/webapp/content/service/appeal/model.js
+++ ace/ace-portal/ace-portal-web/src/main/webapp/content/service/appeal/model.js
@@ -1,43 +1,214 @@
-var _colNames =['主键','诉求名称','封面','二维码','对应处理人openId','答复消息模板','诉求简介','状态','创建人编号','创建人姓名','入库日期','最后更新人编码','最后更新人姓名','最后更新时间','操作'];
-var _colModel = function() {
-	return [
-	{name : 'id',editable : true,width : 100,editoptions : {size : "20",maxlength : "50"},formoptions : {elmprefix : "",elmsuffix : "<span style='color:red;font-size:16px;font-weight:800'>*</span>"},editrules : {required : true}},{name : 'name',editable : true,width : 100,editoptions : {size : "20",maxlength : "50"},formoptions : {elmprefix : "",elmsuffix : "<span style='color:red;font-size:16px;font-weight:800'>*</span>"},editrules : {required : true}},{name : 'cover',editable : true,width : 100,editoptions : {size : "20",maxlength : "50"},formoptions : {elmprefix : "",elmsuffix : "<span style='color:red;font-size:16px;font-weight:800'>*</span>"},editrules : {required : true}},{name : 'qrcoteUrl',editable : true,width : 100,editoptions : {size : "20",maxlength : "50"},formoptions : {elmprefix : "",elmsuffix : "<span style='color:red;font-size:16px;font-weight:800'>*</span>"},editrules : {required : true}},{name : 'openId',editable : true,width : 100,editoptions : {size : "20",maxlength : "50"},},{name : 'tplCode',editable : true,width : 100,editoptions : {size : "20",maxlength : "50"},},{name : 'remark',editable : true,width : 100,editoptions : {size : "20",maxlength : "50"},formoptions : {elmprefix : "",elmsuffix : "<span style='color:red;font-size:16px;font-weight:800'>*</span>"},editrules : {required : true}},{name : 'status',editable : true,width : 100,edittype : "checkbox",editoptions : {value : "1:0"},cellattr : function(rowId, val, rawObject, cm, rdata) {if (rawObject.status == '0') {return "style='background:red;color:#fff'";}if (rawObject.status == '1') {return "style='background:#FF9224;color:#000000'";}if (rawObject.status == '2') {return "style='background:green;color:#fff'";}if (rawObject.status == '3') {return "style='background:#F9F900;color:#000000'";}if (rawObject.status == '4') {return "style='background:#FF9224;color:#000000'";}},unformat : aceSwitch,renderer : function(value) {var rst = "";switch (value) {case '1' :rst = "ON";break;case '0' :rst = "OFF";break;default :rst = "N/A";}return rst;},formoptions : {elmprefix : "",elmsuffix : "<span style='color:red;font-size:16px;font-weight:800'>*</span>"},editrules : {required : true}},{name : 'createUserId',hidden : true,editable : false,width : 100},{name : 'createUserName',hidden : true,editable : false,width : 100},{name : 'createDate',hidden : true,editable : false,width : 100},{name : 'lastModifyUserId',hidden : true,editable : false,width : 100},{name : 'lastModifyUserName',hidden : true,editable : false,width : 100},{name : 'lastModifyDate',hidden : true,editable : false,width : 100},
-	 {
-                name: 'opt',
-                width: 100,
-                hidden:false,
-                editable: false,
-                sortable:false,
-                renderer:function(value, cur){
-                    return renderBtn(cur);
-                }
+var _colNames = ['主键', '诉求名称', '封面', '二维码', '处理人', '答复消息模板', '诉求简介', '状态', '创建人编号', '创建人姓名', '入库日期', '最后更新人编码',
+    '最后更新人姓名', '最后更新时间'
+];
+var _colModel = function () {
+    return [{
+            name: 'id',
+            editable: false,
+            hidden: true,
+            width: 100,
+            editoptions: {
+                size: "20",
+                maxlength: "50"
+            },
+            formoptions: {
+                elmprefix: "",
+                elmsuffix: "<span style='color:red;font-size:16px;font-weight:800'>*</span>"
+            },
+            editrules: {
+                required: true
             }
-	];
+        }, {
+            name: 'name',
+            editable: true,
+            width: 600,
+            editoptions: {
+                style: "width:400px",
+                colspan: true,
+                maxlength: "50"
+            },
+            formoptions: {
+                elmprefix: "",
+                elmsuffix: "<span style='color:red;font-size:16px;font-weight:800'>*</span>"
+            },
+            editrules: {
+                required: true
+            }
+        }, {
+            name: 'cover',
+            editable: true,
+            hidden: true,
+            width: 100,
+            editoptions: {
+                style: "width:400px",
+                colspan: true
+            },
+            formoptions: {
+                elmprefix: "",
+                elmsuffix: "<span style='color:red;font-size:16px;font-weight:800'>*</span>"
+            },
+            editrules: {
+                required: true
+            }
+        }, {
+            name: 'qrcoteUrl',
+            hidden: true,
+            editable: true,
+            width: 100,
+            editoptions: {
+                style: "width:400px",
+                colspan: true
+            },
+            formoptions: {
+                elmprefix: "",
+                elmsuffix: "<span style='color:red;font-size:16px;font-weight:800'>*</span>"
+            },
+            editrules: {
+                required: true
+            }
+        }, {
+            name: 'openId',
+            hidden: true,
+            editable: true,
+            width: 100,
+            editoptions: {
+                size: "20",
+                maxlength: "50"
+            },
+        }, {
+            name: 'tplCode',
+            hidden: true,
+            editable: true,
+            width: 100,
+            editoptions: {
+                size: "20",
+                maxlength: "50"
+            },
+        }, {
+            name: 'remark',
+            hidden: true,
+            edittype: 'textarea',
+            editable: true,
+            width: 100,
+            editoptions: {
+                colspan: true,
+				rows:"10" ,
+				cols:"63"
+            },
+            formoptions: {
+                elmprefix: "",
+                elmsuffix: "<span style='color:red;font-size:16px;font-weight:800'>*</span>"
+            },
+            editrules: {
+                required: true
+            }
+        }, {
+            name: 'status',
+            editable: true,
+            width: 50,
+            edittype: "checkbox",
+            editoptions: {
+                value: "1:0"
+            },
+            cellattr: function (rowId, val, rawObject, cm, rdata) {
+                if (rawObject.status == '0') {
+                    return "style='background:red;color:#fff'";
+                }
+                if (rawObject.status == '1') {
+                    return "style='background:green;color:#fff'";
+                }
+                if (rawObject.status == '2') {
+                    return "style='background:green;color:#fff'";
+                }
+                if (rawObject.status == '3') {
+                    return "style='background:#F9F900;color:#000000'";
+                }
+                if (rawObject.status == '4') {
+                    return "style='background:#FF9224;color:#000000'";
+                }
+            },
+            unformat: aceSwitch,
+            renderer: function (value) {
+                var rst = "";
+                switch (value) {
+                    case '1':
+                        rst = "ON";
+                        break;
+                    case '0':
+                        rst = "OFF";
+                        break;
+                    default:
+                        rst = "N/A";
+                }
+                return rst;
+            },
+
+            formoptions: {
+                elmprefix: "",
+                elmsuffix: "<span style='color:red;font-size:16px;font-weight:800'>*</span>"
+            },
+            editrules: {
+                required: true
+            }
+        }, {
+            name: 'createUserId',
+            hidden: true,
+            editable: false,
+            width: 100
+        }, {
+            name: 'createUserName',
+            hidden: true,
+            editable: false,
+            width: 100
+        }, {
+            name: 'createDate',
+            hidden: true,
+            editable: false,
+            width: 100
+        }, {
+            name: 'lastModifyUserId',
+            hidden: true,
+            editable: false,
+            width: 100
+        }, {
+            name: 'lastModifyUserName',
+            hidden: true,
+            editable: false,
+            width: 100
+        }, {
+            name: 'lastModifyDate',
+            hidden: true,
+            editable: false,
+            width: 100
+        }
+    ];
 }
+
 function aceSwitch(cellvalue, options, cell) {
-	console.log('aceSwitch');
-	setTimeout(function() {
-		$(cell).find('input[type=checkbox]').addClass(
-				'ace ace-switch ace-switch-5').after(
-				'<span class="lbl"></span>');
-	}, 0);
+    console.log('aceSwitch');
+    setTimeout(function () {
+        $(cell).find('input[type=checkbox]').addClass(
+            'ace ace-switch ace-switch-5').after(
+            '<span class="lbl"></span>');
+    }, 0);
 }
 // enable datepicker
 function pickDate(cellvalue, options, cell) {
-	setTimeout(function() {
-		$(cell).find('input[type=text]').datepicker({
-			format : 'yyyy-mm-dd',
-			autoclose : true
-		});
-	}, 0);
+    setTimeout(function () {
+        $(cell).find('input[type=text]').datepicker({
+            format: 'yyyy-mm-dd',
+            autoclose: true
+        });
+    }, 0);
 }
+
 function renderBtn(cur) {
-	var id = $.jgrid.getAccessor(cur, 'id');
-	var title = $.jgrid.getAccessor(cur, 'name');
-	var html = [];
-	html.push('<a target="_blank" href="');
-	html.push('javascript:preview(\'' + id + '\',\'' + title + '\')');
-	html.push('"');
-	html.push('><span class="badge badge-info">查看</span></a>');
-	return html.join(' ');
+    var id = $.jgrid.getAccessor(cur, 'id');
+    var title = $.jgrid.getAccessor(cur, 'name');
+    var html = [];
+    html.push('<a target="_blank" href="');
+    html.push('javascript:preview(\'' + id + '\',\'' + title + '\')');
+    html.push('"');
+    html.push('><span class="badge badge-info">查看</span></a>');
+    return html.join(' ');
 }
