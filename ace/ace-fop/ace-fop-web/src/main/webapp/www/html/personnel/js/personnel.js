263913112f1b9e95a8630728008ea965a9583702
249134995@qq.com <249134995@qq.com>
2018-05-18 11:47:57 +0800

    工商联-人才信息发布

--- ace/ace-fop/ace-fop-web/src/main/webapp/www/html/personnel/js/personnel.js
+++ ace/ace-fop/ace-fop-web/src/main/webapp/www/html/personnel/js/personnel.js
@@ -6,6 +6,12 @@ var currentPage = 1;
 var app =angular.module(ngAppName, []);
 
 app.controller(ngControllerName,function($scope){
+    //初始化记事本
+    var editor = new Simditor({
+        textarea: $('#editor')
+
+    });
+
     $.ajax({
         url: "/fop/www/findInformationServiceListDo",
         type:"post",
@@ -83,5 +89,59 @@ app.controller(ngControllerName,function($scope){
                 skin: 'myskin'
             });
         }
+    }
+
+    /**
+     * 发布人才信息
+     */
+    $scope.releaseInfo = function(){
+        var title = $("input[name='postname']").val();
+        var content = $("textarea[name='content']").val();
+        if(title == '' || title == undefined){
+            layer.alert("岗位名称不能为空！", {
+                icon: 5,
+                skin: 'myskin'
+            });
+            return;
+        }
+        if(content == '' || content == undefined){
+            layer.alert("岗位具体内容不能为空！", {
+                icon: 5,
+                skin: 'myskin'
+            });
+            return;
+        }
+        $.ajax({
+            url: "/fop/www/insertInformationServiceDo",
+            type:"post",
+            async:false,
+            data:{title: title, content: content, modules: "3"},
+            success:function(result){
+                if(result.status == 0) {
+                    console.log(result);
+                    $scope.searchList(currentPage, pageSize);
+                    layer.alert("发布成功！", {
+                        icon: 1,
+                        skin: 'myskin'
+                    });
+                    $("input[name='postname']").val("");
+                    $("textarea[name='content']").val("");
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                }else {
+                    layer.alert(result.errorMessage, {
+                        icon: 5,
+                        skin: 'myskin'
+                    });
+                }
+            },
+            error:function(){
+                layer.alert("系统内部服务异常！", {
+                    icon: 5,
+                    skin: 'myskin'
+                });
+            }
+        });
     }
 });
