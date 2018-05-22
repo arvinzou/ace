4e71f2434c0d47f8e24c9a838ba47250bbf2a650
249134995@qq.com <249134995@qq.com>
2018-05-22 18:41:11 +0800

    工商联-企业产品

---
+++ ace/ace-fop/ace-fop-web/src/main/webapp/www/html/law/js/lawer_doc.js
@@ -0,0 +1,116 @@
+var pageSize=10;
+var ngControllerName = "angularjsCtrl";
+var ngAppName = "angularjsApp";
+var currentPage = 1;
+//angularjs Controller初始化
+var app =angular.module(ngAppName, []);
+
+app.controller(ngControllerName,function($scope){
+    $.ajax({
+        url: "/fop/www/findLawPaperList",
+        type:"post",
+        async:false,
+        data:{"limit":pageSize, page: currentPage},
+        success:function(result){
+            if(result.status == 0) {
+                $scope.items = result.data.list;
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+                var totalSize = result.data.total;
+                var totalPage;
+                if(totalSize % pageSize == 0){
+                    totalPage = totalSize / pageSize;
+                }else{
+                    totalPage = totalSize / pageSize +1;
+                }
+                laypage({
+                    cont: $("#paganation"),   //容器名
+                    pages: totalPage,           //总页数
+                    curr: currentPage,         //当前页
+                    skip: true,
+                    skin: '#1A56A8',
+                    groups: 5,                  //连续显示分页数
+                    jump: function(obj, first){ //触发分页后的回调
+                        if(!first){
+                            currentPage = obj.curr;
+                            $scope.searchList(currentPage, pageSize);
+                        }
+                    }
+
+                });
+            }else {
+                layer.alert(result.errorMessage, {
+                    icon: 5,
+                    skin: 'myskin'
+                });
+            }
+        },
+        error:function(){
+            layer.alert("系统内部服务异常！", {
+                icon: 5,
+                skin: 'myskin'
+            });
+        }
+    });
+
+    $scope.searchList = function(currentPage, pageSize){
+        var key_word = $("#key_word").val();
+        $.ajax({
+            url: "/fop/www/findLawPaperList",
+            type:"post",
+            async:false,
+            data:{page:currentPage, limit: pageSize, title: key_word },
+            success:$scope.responseHandle,
+            error:function(){
+                layer.alert("系统内部服务异常！", {
+                    icon: 5,
+                    skin: 'myskin'
+                });
+            }
+        });
+    }
+
+    $scope.responseHandle = function(result){
+        if(result.status == 0) {
+            $scope.items = result.data.list;
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        }else {
+            layer.alert(result.errorMessage, {
+                icon: 5,
+                skin: 'myskin'
+            });
+        }
+    }
+    $scope.search = function(){
+        var key_word = $("#key_word").val();
+        $.ajax({
+            url: "/fop/www/findLawPaperList",
+            type:"post",
+            async:false,
+            data:{limit:pageSize, page: currentPage, title : key_word},
+            success:function(result){
+                if(result.status == 0) {
+                    $scope.items = result.data.list;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                }else {
+                    layer.alert(result.errorMessage, {
+                        icon: 5,
+                        skin: 'myskin'
+                    });
+                }
+            },
+            error:function(){
+                layer.alert("系统内部服务异常！", {
+                    icon: 5,
+                    skin: 'myskin'
+                });
+            }
+        });
+    }
+
+});
---
+++ ace/ace-fop/ace-fop-web/src/main/webapp/www/html/login/js/regist.js
@@ -0,0 +1,86 @@
+var ngControllerName = "angularjsCtrl";
+var ngAppName = "angularjsApp";
+var app =angular.module(ngAppName, []);
+var flag = null;      //用来标注是企业用户还是团体用户
+
+app.controller(ngControllerName,function($scope) {
+
+    $scope.changeType = function($event, param){
+        var typeDiv = $(".companyType");
+        for(var i=0; i<typeDiv.length; i++){
+            typeDiv[i].checked  = false;
+            $(typeDiv[i]).children("div[name='check']").addClass("default");
+        }
+        ($event.target).checked = true;
+        $($event.target).children("div[name='check']").removeClass("default");
+        console.log(param);
+        if(param){
+            $("#name").text("企业名称");
+        }else{
+            $("#name").text("团体名称");
+        }
+        flag = param;
+    }
+
+    $scope.resigt = function(){
+        var regist_flag = true;
+        var regist_name = $("input[name='regist_name']").val();
+        var regist_num = $("input[name='regist_num']").val();
+        if(flag == null){
+            regist_flag = false;
+            layer.alert("会员类型未选择！", {
+                icon: 5,
+                skin: 'myskin'
+            });
+            return;
+        }
+        if(regist_name == '' || regist_name == undefined){
+            regist_flag = false;
+            layer.alert("注册名称不能为空！", {
+                icon: 5,
+                skin: 'myskin'
+            });
+            return;
+        }
+        if(regist_num == '' || regist_num == undefined){
+            regist_flag = false;
+            layer.alert("账号设置不能为空！", {
+                icon: 5,
+                skin: 'myskin'
+            });
+            return;
+        }
+        if(regist_flag){
+            $.ajax({
+                url: "/fop/www/sign",
+                type: "post",
+                async: false,
+                data: {name: regist_name, phoneNumber: regist_num, isCompany: flag},
+                success: function (result) {
+                    if (result.status == 0) {
+                       console.log(result);
+                        layer.alert("注册成功！", {
+                            icon: 1,
+                            skin: 'myskin'
+                        });
+                       location.href = "/portal/dynamic/portal/security/login.jsp";
+                        if (!$scope.$$phase) {
+                            $scope.$apply();
+                        }
+                    } else {
+                        layer.alert(result.errorMessage, {
+                            icon: 5,
+                            skin: 'myskin'
+                        });
+                    }
+                },
+                error: function () {
+                    layer.alert("系统内部服务异常！", {
+                        icon: 5,
+                        skin: 'myskin'
+                    });
+                }
+            });
+        }
+    }
+});
