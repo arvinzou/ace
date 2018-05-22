eecb195dbb4330a47da91700c1c3b7c90bd926f2
249134995 <249134995@qq.com>
2018-05-22 08:47:53 +0800

    诉求管理

--- ace/ace-sapp-fop/config.js
+++ ace/ace-sapp-fop/config.js
@@ -5,15 +5,16 @@ var config = {
     serverfile,
     appid: 'wxcf3d50130f9844f5',
     appsecret: '10a109f311c9803c2a8fd17b14bf09dd',
-    frontColor: '#fff',
+    frontColor: '#1A56A8',
     backgroundColor: "blue",
     loginUrl: `${server}/portal/www/authority.do`,
+    bindUrl: `${server}/portal/www/bind.do`,
     checkImageUrl: `${server}/portal/captcha/image.do`,
     uploadUrl: `${server}/portal/www/upload.do`,
     getAppealById: `${server}/portal/www/appealCase/getAppealById.do`,
     insertAppealCase: `${server}/portal/www/appealCase/insertAppealCase.do`,
     getList: `${server}/portal/www/appealCase/getList.do`,
-    selectAppealCaseByPrimaryKey: `${server}/portal/www/appealCase/selectAppealCaseByPrimaryKey.do`
-
+    selectAppealCaseByPrimaryKey: `${server}/portal/www/appealCase/selectAppealCaseByPrimaryKey.do`,
+    sendCmccByMobile: `${server}/portal/www/sendCmccByMobile.do`,
 };
 module.exports = config
