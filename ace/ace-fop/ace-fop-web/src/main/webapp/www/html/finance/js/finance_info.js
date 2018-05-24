fbbc0aed18e068b4cdc607994ffffdceea83acef
249134995@qq.com <249134995@qq.com>
2018-05-24 11:24:11 +0800

    工商联-罗灿

--- ace/ace-fop/ace-fop-web/src/main/webapp/www/html/finance/js/finance_info.js
+++ ace/ace-fop/ace-fop-web/src/main/webapp/www/html/finance/js/finance_info.js
@@ -11,52 +11,51 @@ app.controller(ngControllerName,function($scope){
     var url =   window.location.search.substring(1);
     var primaryId = url.substring(url.indexOf('=')+1);
     console.log(primaryId);
-    $.ajax({
-        url: "/fop/www/selectFinanceProjectByPrimaryKey",
-        type:"post",
-        async:false,
-        data:{id: primaryId},
-        success:function(result){
-            if(result.status == 0) {
-                $scope.financeInfo = result.data;
-                $scope.comments = result.data.comments;
-                if (!$scope.$$phase) {
-                    $scope.$apply();
-                }
-                if($(".custom_layper_page_info").length>0){
-                    setTimeout(function(){
-                        var itemSize=$(".msg_list table").length;//直接从html取出已经渲染的tr
-                        var pages = Math.ceil(itemSize/pageSize); //得到总页数
-                        $(".custom_layper_page_info").html(pageSize+"条/页，共"+itemSize+"条信息");
-                        laypage({
-                            cont: 'layper_page_box',
-                            pages: pages,
-                            first: false,
-                            last: false,
-                            skip:false,
-                            groups: 8, //连续显示分页数
-                            jump: function(obj){
-                                staticPages(obj.curr);
-                            }
+        $.ajax({
+            url: "/fop/www/selectFinanceProjectByPrimaryKey",
+            type:"post",
+            async:false,
+            data:{id: primaryId},
+            success:function(result){
+                if(result.status == 0) {
+                    $scope.financeInfo = result.data;
+                    $scope.comments = result.data.comments;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                    if($(".custom_layper_page_info").length>0){
+                        setTimeout(function(){
+                            var itemSize=$(".msg_list table").length;//直接从html取出已经渲染的tr
+                            var pages = Math.ceil(itemSize/pageSize); //得到总页数
+                            $(".custom_layper_page_info").html(pageSize+"条/页，共"+itemSize+"条信息");
+                            laypage({
+                                cont: 'layper_page_box',
+                                pages: pages,
+                                first: false,
+                                last: false,
+                                skip:false,
+                                groups: 8, //连续显示分页数
+                                jump: function(obj){
+                                    staticPages(obj.curr);
+                                }
+                            });
+                            staticPages(1);//静态分页
                         });
-                        staticPages(1);//静态分页
+                    }
+                }else {
+                    layer.alert(result.errorMessage, {
+                        icon: 5,
+                        skin: 'myskin'
                     });
                 }
-            }else {
-                layer.alert(result.errorMessage, {
+            },
+            error:function(){
+                layer.alert("系统内部服务异常！", {
                     icon: 5,
                     skin: 'myskin'
                 });
             }
-        },
-        error:function(){
-            layer.alert("系统内部服务异常！", {
-                icon: 5,
-                skin: 'myskin'
-            });
-        }
-    });
-
+        });
     function staticPages(curr){
         $(".msg_list table").each(function(index){
                 var startNo=parseInt(pageSize)*(curr-1)+1;//5
@@ -67,5 +66,89 @@ app.controller(ngControllerName,function($scope){
                     $(this).hide();
                 }
             });
+    }
+
+    $scope.init = function(){
+        $.ajax({
+            url: "/fop/www/selectFinanceProjectByPrimaryKey",
+            type:"post",
+            async:false,
+            data:{id: primaryId},
+            success:function(result){
+                if(result.status == 0) {
+                    $scope.financeInfo = result.data;
+                    $scope.comments = result.data.comments;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                    if($(".custom_layper_page_info").length>0){
+                        setTimeout(function(){
+                            var itemSize=$(".msg_list table").length;//直接从html取出已经渲染的tr
+                            var pages = Math.ceil(itemSize/pageSize); //得到总页数
+                            $(".custom_layper_page_info").html(pageSize+"条/页，共"+itemSize+"条信息");
+                            laypage({
+                                cont: 'layper_page_box',
+                                pages: pages,
+                                first: false,
+                                last: false,
+                                skip:false,
+                                groups: 8, //连续显示分页数
+                                jump: function(obj){
+                                    staticPages(obj.curr);
+                                }
+                            });
+                            staticPages(1);//静态分页
+                        });
+                    }
+                }else {
+                    layer.alert(result.errorMessage, {
+                        icon: 5,
+                        skin: 'myskin'
+                    });
+                }
+            },
+            error:function(){
+                layer.alert("系统内部服务异常！", {
+                    icon: 5,
+                    skin: 'myskin'
+                });
+            }
+        });
+    }
+
+    $scope.releaseMsg = function(){
+        var reply = $("#release_msg").val();
+        $.ajax({
+            url: "/fop/www/insertQuestion",
+            type:"post",
+            async:false,
+            data:{parentId: primaryId, sourceType: "2", reply: reply},
+            success:function(result){
+                if(result.status == 0) {
+                   console.log(result);
+                    $("#release_msg").val("");
+                    layer.alert("发表成功！", {
+                        icon: 1,
+                        skin: 'myskin'
+                    });
+                    $scope.init();
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+
+                }else {
+                    layer.alert(result.errorMessage, {
+                        icon: 5,
+                        skin: 'myskin'
+                    });
+                }
+            },
+            error:function(){
+                layer.alert("系统内部服务异常！", {
+                    icon: 5,
+                    skin: 'myskin'
+                });
+            }
+        });
     }
 });
