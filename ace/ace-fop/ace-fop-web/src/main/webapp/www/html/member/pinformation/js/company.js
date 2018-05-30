b38f672afc6ef13a9497543ae0b13d309526a82a
249134995@qq.com <249134995@qq.com>
2018-05-30 18:21:20 +0800

    工商联-罗灿

--- ace/ace-fop/ace-fop-web/src/main/webapp/www/html/member/pinformation/js/company.js
+++ ace/ace-fop/ace-fop-web/src/main/webapp/www/html/member/pinformation/js/company.js
@@ -36,6 +36,7 @@ app.controller(ngControllerName,function($scope){
     });
 
     $scope.insertInformation = function(){
+        /*基本信息*/
         var fullName = $("input[name='fullName']").val();
         var realName = $("input[name='realName']").val();
         var companyType = $("#companyType option:checked").val();
@@ -45,7 +46,8 @@ app.controller(ngControllerName,function($scope){
         var fixedAssets = $("input[name='fixedAssets']").val();
         var workingCapital = $("input[name='workingCapital']").val();
         var address = $("input[name='address']").val();
-        var realName = $("input[name='realName']").val();      //重复
+
+        /*法人代表*/
         var sex = $("#sex option:checked").val();
         var birthDate = $("input[name='birthDate']").val();
         var nativePlace = $("input[name='nativePlace']").val();
@@ -61,28 +63,113 @@ app.controller(ngControllerName,function($scope){
         var email = $("input[name='email']").val();
         var resume = $("textarea[name='resume']").val();
         var achievement = $("textarea[name='achievement']").val();
+
+        /*企业生产经营信息*/
         var ownSpace = $("input[name='ownSpace']").val();
         var tenancySpace = $("input[name='tenancySpace']").val();
         var majorVariety = $("textarea[name='majorVariety']").val();
+
+        /*党组织信息*/
         var companyOrgType = $("input[name='companyOrgType']").val();
         var peopleNum = $("input[name='peopleNum']").val();
-        var establishDate = $("input[name='establishDate']").val();
+        var dzz_establishDate = $("input[name='dzz_establishDate']").val();
         var dutyPersonName = $("input[name='dutyPersonName']").val();    //党组织负责人
         var dutyPersonPhone = $("input[name='dutyPersonPhone']").val();  //负责人联系电话
+
+        /*工会组织信息*/
         var gh_establishDate = $("input[name='gh_establishDate']").val();//工会组织建立时间
         var gh_dutyPersonName = $("input[name='gh_dutyPersonName']").val();//工会组织负责人
         var gh_dutyPersonPhone = $("input[name='gh_dutyPersonPhone']").val();//工会联系电话
+
+        /*构建和谐劳动关系*/
         var laborContractNum = $("input[name='laborContractNum']").val();
         var thirdLaborRelation = $("#thirdLaborRelation option:checked").val();
         var socialInsuranceAddr = $("input[name='socialInsuranceAddr']").val();
         var socialInsuranceNum = $("input[name='socialInsuranceNum']").val();
+
+        /*对社会公益事业做过何种贡献*/
         var xg_work = $("textarea[name='xg_work']").val();     //安排下岗职工再就业
         var education = $("textarea[name='education']").val(); //助学兴教
         var poverty = $("textarea[name='poverty']").val();     //帮困扶贫
         var others = $("textarea[name='others']").val();       //其他
+
+        /*纳税情况*/
         var accTaxAmount = $("input[name='accTaxAmount']").val();
         var yearTaxAmount = $("input[name='yearTaxAmount']").val();
 
+        var data = {
+            "basicInfo":{
+                "fullName": fullName,
+                "realName": realName,
+                "companyType": companyType,
+                "establishDate": establishDate,
+                "crewSize": crewSize,
+                "registeredCapital": registeredCapital,
+                "fixedAssets": fixedAssets,
+                "workingCapital": workingCapital,
+                "address": address,
+                "ownSpace":ownSpace,
+                "tenancySpace":tenancySpace,
+                "majorVariety":majorVariety,
+                "laborContractNum":laborContractNum,
+                "thirdLaborRelation":thirdLaborRelation,
+                "socialInsuranceAddr":socialInsuranceAddr,
+                "socialInsuranceNum":socialInsuranceNum,
+                "accTaxAmount":accTaxAmount,
+                "yearTaxAmount":yearTaxAmount
+            },
+            "legalPerson":{
+                "sex": sex,
+                "birthDate": birthDate,
+                "nativePlace": nativePlace,
+                "nationality": nationality,
+                "political": political,
+                "recruitmentDate": recruitmentDate,
+                "education": education,
+                "skillJobTitle": skillJobTitle,
+                "deptPost": deptPost,
+                "societyPost":societyPost,
+                "postcode": postcode,
+                "fax":fax,
+                "email":email,
+                "resume":resume,
+                "achievement":achievement
+            },
+            "dang":{
+                "companyOrgType":companyOrgType,
+                "peopleNum":peopleNum,
+                "establishDate":dzz_establishDate,
+                "dutyPersonName":dutyPersonName,
+                "dutyPersonPhone":dutyPersonPhone
+            },
+            "gonghui":{
+                "establishDate":gh_establishDate,
+                "dutyPersonName":gh_dutyPersonName,
+                "dutyPersonPhone":gh_dutyPersonPhone,
+            },
+            "contribute":[
+                {
+                    "itemCode": "1",
+                    "itemName":"安排下岗职工再就业",
+                    "itemValue":xg_work
+                },
+                {
+                    "itemCode": "2",
+                    "itemName":"助学兴教",
+                    "itemValue":education
+                },
+                {
+                    "itemCode": "3",
+                    "itemName":"帮困扶贫",
+                    "itemValue":poverty
+                },
+                {
+                    "itemCode": "4",
+                    "itemName":"其他",
+                    "itemValue":others
+                }
+            ]
+        }
         $.ajax({
             url: "/fop/www/insertAssociationInfo",
             type:"post",
