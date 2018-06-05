36e2f50f594c6a12fa471d1d82aca68109d8a169
249134995@qq.com <249134995@qq.com>
2018-06-05 16:05:23 +0800

    工商联-罗灿

--- ace/ace-fop/ace-fop-web/src/main/webapp/www/html/leader/js/leader_index.js
+++ ace/ace-fop/ace-fop-web/src/main/webapp/www/html/leader/js/leader_index.js
@@ -6,3 +6,12 @@ var app =angular.module(ngAppName, []);
 app.controller(ngControllerName,function($scope){
 
 });
+
+function setIframeHeight(iframe) {
+    if(iframe) {
+        var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
+        if(iframeWin.document.body) {
+            iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
+        }
+    }
+};
