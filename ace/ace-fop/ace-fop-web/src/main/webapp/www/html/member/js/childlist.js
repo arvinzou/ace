ce282c3f0ed328db4a55024895ae649a607a191f
249134995@qq.com <249134995@qq.com>
2018-06-05 11:13:35 +0800

    工商联-页面修改-罗灿

--- ace/ace-fop/ace-fop-web/src/main/webapp/www/html/member/js/childlist.js
+++ ace/ace-fop/ace-fop-web/src/main/webapp/www/html/member/js/childlist.js
@@ -1,4 +1,7 @@
 $(function(){
+    parent.document.getElementById("iframe").height= 700;
+    parent.document.getElementById("iframe").height=document.documentElement.scrollHeight;
+
     $(".menu-nav ul li a").eq(0).addClass("checked");
     $(".menu-nav ul li").hover(function(){
         $(this).find("a").addClass("checked");
