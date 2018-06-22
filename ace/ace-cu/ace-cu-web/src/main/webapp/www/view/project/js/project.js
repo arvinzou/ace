cdfb40f7890d1e52a2237bf63ba0a1b90e4ba5aa
249134995@qq.com <249134995@qq.com>
2018-06-22 11:29:43 +0800

    慈善总会-罗灿

--- ace/ace-cu/ace-cu-web/src/main/webapp/www/view/project/js/project.js
+++ ace/ace-cu/ace-cu-web/src/main/webapp/www/view/project/js/project.js
@@ -4,6 +4,7 @@ var hostPath = "http://127.0.0.1";
 var page = 0;
 var pageSize = 9999;
 var app =angular.module(ngAppName, []);
+var defaultProjectId = null;
 
 app.controller(ngControllerName,function($scope){
 	
@@ -16,6 +17,7 @@ app.controller(ngControllerName,function($scope){
             if(result.status == 0) {
                 $scope.items = result.data.rows;
                 $scope.projectInfo = result.data.rows[0];
+                defaultProjectId = result.data.rows[0].id;
     			$("#projectDetail").html(result.data.rows[0].description);
                 if (!$scope.$$phase) {
                     $scope.$apply();
@@ -28,7 +30,47 @@ app.controller(ngControllerName,function($scope){
             alert("系统服务内部异常！");
         }
     });
-    
+
+    $.ajax({
+        url: "/cu/www/project/findUsedRecordList",
+        type:"post",
+        async:false,
+        data:{projectId: defaultProjectId, start: 0, limit: 9999},
+        success:function(result){
+            if(result.status == 0) {
+                $scope.useRecords = result.data.rows;
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+            }else {
+                alert(errorMessage);
+            }
+        },
+        error:function(){
+            alert("系统服务内部异常！");
+        }
+    });
+
+    $.ajax({
+        url:"/cu/www/project/findDonateList",
+        type:"post",
+        async:false,
+        data:{projectId: defaultProjectId, start: 0, limit: 9999},
+        success:function(result){
+            if(result.status == 0) {
+                $scope.donationList = result.data.rows;
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+            }else {
+                alert(errorMessage);
+            }
+        },
+        error:function(){
+            alert("系统服务内部异常！");
+        }
+    });
+
     $scope.showInfo = function(id){
     	var datas = $scope.items;
     	for(var i=0; i<datas.length; i++){
