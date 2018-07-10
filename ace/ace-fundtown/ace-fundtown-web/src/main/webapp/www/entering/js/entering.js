e24049f343e0d48bc8b71ebdd6a9f70aeaf97373
huacai003 <249134995@qq.com>
2018-07-10 16:55:52 +0800

    品武陵后台——王恩

---
+++ ace/ace-fundtown/ace-fundtown-web/src/main/webapp/www/entering/js/entering.js
@@ -0,0 +1,50 @@
+$(function () {
+    initWeb();
+    $('.btn_list').on('click', 'li', linkurl);
+})
+
+function initWeb() {
+    var url = '/fundtown/www/process/getNodeList';
+    $.getJSON(url, function (result) {
+        if (result.status == 0) {
+            viewNodeList(result.data);
+            return;
+        }
+        alert("网络异常~刷新重试。")
+    })
+}
+
+function viewNodeList(datas) {
+    $('#node_list').empty();
+    for (var i = 0; i < datas.length; i++) {
+        var temp = nodeTemplate;
+        var data = datas[i];
+        temp = temp.replace('[sequence]', data.sequence)
+            .replace('[curNodeName]', data.curNodeName);
+        var obj = {
+            id: data.id,
+            linkUrl: data.linkUrl
+        }
+        $('#node_list').append($(temp).data(obj));
+    }
+}
+
+function linkurl() {
+    var $that = $(this);
+    var obj = $that.data();
+    if (window.localStorage) {
+        //存储变量的值
+        localStorage.id = obj['id'];
+        location.href = '../approved/' + obj['linkUrl'];
+    } else {
+        alert("NOT SUPPORT");
+    }
+}
+
+var nodeTemplate = '<li>' +
+    '            <div class="left">' +
+    '                <span class="num">0[sequence]</span>' +
+    '                <span class="name">[curNodeName]</span>' +
+    '            </div>' +
+    '            <div class="right">查看</div>' +
+    '        </li>';
---
+++ ace/ace-fundtown/ace-fundtown-web/src/main/webapp/www/js/init-wx.js
@@ -0,0 +1,10 @@
+$(function () {
+    var url = '/portal/www/wx/jsapi/getConfig';
+    var data = {
+        url: "http://zx.huacainfo.com",
+        sysId: 'fundtown'
+    }
+    $.getJSON(url, data, function (result) {
+
+    })
+})
