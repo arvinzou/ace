a21c6335a90823775c1f59fbad7ce5f9d017ba55
huacai003 <249134995@qq.com>
2018-07-10 09:29:36 +0800

    品武陵后台——王恩

---
+++ ace/ace-fundtown/ace-fundtown-web/src/main/webapp/www/map/js/data.js
@@ -0,0 +1,199 @@
+var youle = [{
+    name: "欢乐水世界",
+    value: [111.759742, 29.084898]
+},
+    {
+        name: "沙滩公园",
+        value: [111.737953, 29.120235]
+    },
+    {
+        name: "游船码头",
+        value: [111.741363, 29.063861]
+    },
+    {
+        name: "司马楼",
+        value: [111.748695, 29.073628]
+    },
+    {
+        name: "柳叶风帆",
+        value: [111.744606, 29.070259]
+    },
+    {
+        name: "湖仙岛",
+        value: [111.744519, 29.063275]
+    },
+    {
+        name: "常德体育生态园",
+        value: [111.71304, 29.12212]
+    },
+];
+var jingdian = [{
+    name: "太阳山森林公园",
+    value: [111.717617, 29.162507]
+},
+    {
+        name: "河街",
+        value: [111.723703, 29.057381]
+    },
+    {
+        name: "德国风情街",
+        value: [111.716152, 29.058113]
+    },
+    {
+        name: "湖南桃花源国际高尔夫俱乐部",
+        value: [111.702688, 29.120631]
+    },
+];
+var self = [{
+    name: "清科小镇",
+    value: [111.737953, 29.057274, 9]
+}]
+var shisu = [{
+    name: "共和酒店",
+    value: [111.739109, 29.059183]
+},
+    {
+        name: "华尔夫酒店",
+        value: [111.734274, 29.061565]
+    },
+    {
+        name: "柳园锦江酒店",
+        value: [111.740883, 29.104013]
+    },
+];
+var yiyuan = [{
+    name: "常德湘雅医院",
+    value: [111.709034, 29.097186]
+},]
+var jiaoyu = [{
+    name: "柳叶湖学校",
+    value: [111.736953, 29.074768]
+},
+    {
+        name: "湖南省水上教育学校",
+        value: [111.737819, 29.060702]
+    },
+]
+
+var massage = {
+    欢乐水世界: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/ssj/1.jpg',
+            'img/demo/微信截图_20180202130818.png',
+            'img/demo/05620f7fd5d8f33f5bf0da215d49abce.jpg'
+        ]
+    },
+    沙滩公园: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/stgy/1.jpg',
+            'img/stgy/2.jpg'
+        ]
+    },
+    游船码头: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/ytmt/1.jpg',
+            'img/ytmt/2.jpg'
+        ]
+    },
+    司马楼: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/sml/1.jpg',
+            'img/sml/2.jpg'
+        ]
+    },
+    柳叶风帆: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/lyff/1.jpg'
+        ]
+    },
+    湖仙岛: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/hxd/1.png'
+        ]
+    },
+    常德体育生态园: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/tyst/1.jpg'
+        ]
+    },
+    太阳山森林公园: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/tys/1.jpg'
+        ]
+    },
+    河街: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/hj/1.jpg',
+            'img/hj/2.png',
+            'img/hj/3.png'
+        ]
+    },
+    德国风情街: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/fqj/1.jpg'
+        ]
+    },
+    湖南桃花源国际高尔夫俱乐部: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/gef/1.jpg'
+        ]
+    },
+    共和酒店: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/gh/1.jpg'
+        ]
+    },
+    华尔夫酒店: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/hf/1.jpg'
+        ]
+    },
+    柳园锦江酒店: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/jj/1.jpg'
+        ]
+    },
+    常德湘雅医院: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/xyyy/1.png',
+            'img/xyyy/2.png',
+            'img/xyyy/3.png'
+        ]
+    },
+    欢乐水世界: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/ssj/1.jpg',
+            'img/demo/微信截图_20180202130818.png',
+            'img/demo/05620f7fd5d8f33f5bf0da215d49abce.jpg'
+        ]
+    },
+    柳叶湖学校: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/lyh/1.png'
+        ]
+    },
+    湖南省水上教育学校: {
+        address: '常德市武陵区环湖路',
+        images: [
+            'img/ssjy/1.jpg',
+            'img/ssjy/2.jpg'
+        ]
+    },
+}
