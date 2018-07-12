6f8a72f90a04794c0b4caf982c3d035df1934585
249134995 <249134995@qq.com>
2018-07-12 22:19:40 +0800

    工商联公众号绑定系统账号

---
+++ ace/ace-jxb/ace-jxb-client/src/main/java/com/huacainfo/ace/jxb/service/RegService.java
@@ -0,0 +1,11 @@
+package com.huacainfo.ace.jxb.service;
+
+import com.huacainfo.ace.common.result.MessageResponse;
+import  com.huacainfo.ace.jxb.model.Reg;
+
+
+public interface RegService {
+
+    MessageResponse insertReg(Reg o) throws Exception;
+
+}
