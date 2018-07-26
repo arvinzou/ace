81b6fa0e5fdb3931e60ba46378d16e93a6ab0a51
249134995@qq.com <249134995@qq.com>
2018-07-26 11:31:59 +0800

    近心帮-罗灿

--- ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/mine/js/mine.js
+++ ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/mine/js/mine.js
@@ -11,7 +11,11 @@ function initData(){
         data:{},
         success:function(result){
             if(result.status == 0) {
-                console.log(result);
+                var myInfo = document.getElementById('myInfo').innerHTML;
+                var html = juicer(myInfo, {
+                    data: result.data
+                });
+                $("#mine").append(html);
             }else {
                 if(result.data == 'unregister'){   //跳转到注册页面
                     window.location.href = '/jxb/www/view/regist/regist.html';
