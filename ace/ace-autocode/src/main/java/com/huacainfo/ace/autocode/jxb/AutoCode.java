478a33a09771774ccfb19b4985de4178ceefd244
249134995@qq.com <249134995@qq.com>
2018-08-06 16:01:05 +0800

    autoCode - 自动构建

--- ace/ace-autocode/src/main/java/com/huacainfo/ace/autocode/jxb/AutoCode.java
+++ ace/ace-autocode/src/main/java/com/huacainfo/ace/autocode/jxb/AutoCode.java
@@ -5,10 +5,25 @@ import com.huacainfo.ace.autocode.base.AutoCodeUtils;
 public class AutoCode {
 
     public static void main(String[] args) throws Exception {
-        AutoCodeUtils autoCodeUtils = new AutoCodeUtils("jxb", "咨询师", "Arvin");
-//        Class<?> c = com.huacainfo.ace.jxb.model.Counselor.class;
-        Class<?> c = null;
+        AutoCodeUtils autoCodeUtils = new AutoCodeUtils("jxb", "课程资源", "Arvin");
+        Class<?> c = com.huacainfo.ace.jxb.model.CourseSource.class;
+//        Class<?> c = null;
         autoCodeUtils.init(c);
+
+//
+//===================================
+//        autoCodeUtils.createVO(c);
+//        autoCodeUtils.createQVO(c);
+//        autoCodeUtils.createDAO(c);
+//        autoCodeUtils.createService(c);
+//        autoCodeUtils.createServiceImpl(c);
+//        autoCodeUtils.createAction(c);
+//        autoCodeUtils.createJsp(c);
+//        autoCodeUtils.createJsConfig(c);
+//        autoCodeUtils.createJsView(c);
+//        autoCodeUtils.createJsController(c);
+//        autoCodeUtils.createModel(c);
+
 
 //===================================
         autoCodeUtils.createVO(c);
@@ -17,19 +32,6 @@ public class AutoCode {
         autoCodeUtils.createService(c);
         autoCodeUtils.createServiceImpl(c);
         autoCodeUtils.createAction(c);
-        autoCodeUtils.createJsp(c);
-        autoCodeUtils.createJsConfig(c);
-        autoCodeUtils.createJsView(c);
-        autoCodeUtils.createJsController(c);
-        autoCodeUtils.createModel(c);
-
-
-//===================================
-//        autoCodeUtils.createVO(c);
-//        autoCodeUtils.createQVO(c);
-//        autoCodeUtils.createDAO(c);
-//        autoCodeUtils.createService(c);
-//        autoCodeUtils.createServiceImpl(c);
 
     }
 
