61923e9b3e3295cfe7533c648d038103daeccf7c
huacai003 <249134995@qq.com>
2018-08-13 17:09:10 +0800

    近兴帮后台

---
+++ ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/test/js/testIndex.js
@@ -0,0 +1,136 @@
+var bannerSwiper, menuSwiper;
+
+var imgMap = {
+    人格测试: 'img/rgcs.png',
+    智力测试: 'img/zlcs.png',
+    行为测试: 'img/xwcs.png',
+    心理健康测试: 'img/xlcs.png',
+    情绪测试: 'img/qxcs.png',
+    人际关系测试: 'img/rjgx.png',
+    兴趣爱好测试: 'img/xqah.png',
+    网络成瘾测试: 'img/wlcy.png'
+}
+$(function () {
+    initSwriper();
+    initMenu();
+    initList();
+    $('.menu .swiper-container_menu').on('click', 'li', sameTestTypeList);
+});
+
+
+function sameTestTypeList() {
+    var $that = $(this);
+    var id = $that.data('id');
+    if (id) {
+        localStorage.removeItem('category');
+        window.location.href = 'testlist.html?id=' + id;
+    }
+}
+
+
+/*初始化测试列表*/
+function initList() {
+    var url = '/portal/www/test/getEvaluatTplList.do';
+    var data = {
+        page: 1,
+        limit: 20
+    };
+    $.getJSON(url, data, function (result) {
+        if (result.status == 0) {
+            viewBanner(result.data.slice(0, 5));
+            viewList(result.data.slice(5));
+        }
+    });
+}
+
+/*初始化列表*/
+
+function viewList(data) {
+    var info = document.getElementById('temp_testLists').innerHTML;
+    var infohtml = juicer(info, {
+        data: data,
+    });
+    $("#testLists").html(infohtml);
+}
+
+/*初始化banner*/
+function viewBanner(data) {
+    for (var i = 0; i < data.length; i++) {
+        var img = bannerTemplate;
+        img = img.replace('[imgUrl]', data[i].cover).replace('[id]', data[i].id);
+        bannerSwiper[0].addSlide(i, img);
+    }
+}
+
+/*初始化Swriper*/
+function initSwriper() {
+    bannerSwiper = new Swiper('.swiper-container_panel', {
+        loop: true,
+        pagination: {
+            el: '.swiper-container_panel .swiper-pagination',
+        },
+    })
+    menuSwiper = new Swiper('.swiper-container_menu', {
+        loop: true,
+        pagination: {
+            el: '.swiper-container_menu .swiper-pagination',
+        },
+    })
+}
+
+/*获取菜单数据*/
+function initMenu() {
+    var url = '/portal/www/test/selectTestTypeList.do';
+    $.getJSON(url, function (result) {
+        viewMenu(result.rows);
+    })
+}
+
+/*渲染菜单*/
+function viewMenu(data) {
+    var index = data.length;
+    var j = 0;
+    var z = 0;
+    var slide = menuTemplateStart;
+    for (var i = 0; i < index; i++) {
+        var li = menuTemplateli;
+        li = li.replace('[menuName]', data[i].name)
+            .replace('[menuId]', data[i].id)
+            .replace('[end]', j == 3 ? 'end' : '')
+            .replace('[urlImg]', imgMap[data[i].name]);
+        slide = slide + li;
+        li = menuTemplateli;
+        j++;
+        if (j == 4) {
+            slide = slide + menuTemplateEnd;
+            menuSwiper[0].addSlide(z, slide);
+            slide = menuTemplateStart;
+            j = 0;
+            z++;
+        }
+    }
+    if (j != 0) {
+        slide = slide + menuTemplateEnd;
+        menuSwiper[0].addSlide(z, slide);
+    }
+}
+
+var bannerTemplate = '<div class="swiper-slide" data-id="[id]"><img src="[imgUrl]"></div>';
+
+var menuTemplateStart = '<div class="swiper-slide">' +
+    '                        <ul class="page">';
+
+var menuTemplateEnd = '</ul>' +
+    '                    </div>';
+
+var menuTemplateli = '<li class="[end]" data-id="[menuId]">' +
+    '                                <div>' +
+    '                                    <img src="[urlImg]"/>' +
+    '                                </div>' +
+    '                                <p>[menuName]</p>' +
+    '                            </li>';
+
+
+
+
+
--- ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/test/js/testlist.js
+++ ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/test/js/testlist.js
@@ -1,10 +1,99 @@
+var mySwiper, category;
 $(function () {
+    initCategory();
+    initSwriper();
+    initMenu();
     /*顶部点击菜单*/
     $('.menu').on('click', '.swiper-slide', changeTestType);
     $('.sort').on('click', '.sortoption', showSort);
     $('.cover').on('click', hiddenSort);
-    $('.sort').on('click', '.option', changeSort)
-})
+    $('.sort').on('click', '.option', changeSort);
+});
+
+function initCategory() {
+    category = localStorage.getItem('category') ? localStorage.getItem('category') : parseQueryString("id");
+}
+
+
+/*初始化测试列表*/
+function initList() {
+    var url = '/portal/www/test/getEvaluatTplList.do';
+    var data = {
+        category: category,
+        page: 1,
+        limit: 20
+    };
+    $.getJSON(url, data, function (result) {
+        if (result.status == 0) {
+            viewList(result.data);
+        }
+    });
+}
+
+/*初始化列表*/
+
+function viewList(data) {
+    var info = document.getElementById('temp_testLists').innerHTML;
+    var infohtml = juicer(info, {
+        data: data,
+    });
+    $("#testLists").html(infohtml);
+}
+
+
+/*解析地址参数*/
+function parseQueryString(prop) {
+    var obj = {};
+    var url = window.location.href;
+    var start = url.indexOf("?") + 1;
+    var str = url.substr(start);
+    var arr = str.split("&");
+    for (var i = 0; i < arr.length; i++) {
+        var arr2 = arr[i].split("=");
+        obj[arr2[0]] = arr2[1];
+    }
+    return obj[prop];
+}
+
+
+/*获取菜单数据*/
+function initMenu() {
+    var url = '/portal/www/test/selectTestTypeList.do';
+    $.getJSON(url, function (result) {
+        viewMenu(result.rows);
+    })
+    initList(parseQueryString('id'));
+}
+
+/*渲染菜单*/
+function viewMenu(data) {
+    for (var i = 0; i < data.length; i++) {
+        mySwiper.addSlide(i, '<span data-id=' + data[i].id + ' class="swiper-slide ' + (data[i].id == category ? "action" : "") + '">' + data[i].name + '</span>');
+    }
+    silperCenter();
+}
+
+function silperCenter() {
+    var index = $('.swiper-container_menu .action').index();
+    mySwiper.slideTo(index, 1000, false);
+}
+
+
+/*初始化Swriper*/
+function initSwriper() {
+    mySwiper = new Swiper('.swiper-container', {
+        spaceBetween: 20,
+        slidesPerView: 'auto',
+        centeredSlides: true
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        //     hide: false,
+        // },
+        // pagination: {
+        //     el: '.swiper-pagination',
+        // },
+    })
+}
 
 function changeSort() {
     var $that = $(this);
@@ -28,8 +117,10 @@ function showSort() {
 
 function changeTestType() {
     var $that = $(this);
+    category = $that.data("id");
+    localStorage.setItem('category', category)
     $that.siblings().removeClass('action');
     $that.addClass('action');
-    var index = $that.index();
-    console.log(index);
+    silperCenter();
+    initList();
 }
