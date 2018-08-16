c00185221c514388a4efedade2ac4dd043580919
249134995@qq.com <249134995@qq.com>
2018-08-16 11:04:36 +0800

    近心帮 - 课程筛选&课程、课程评论属性调整

--- ace/ace-jxb/ace-jxb-client/src/main/java/com/huacainfo/ace/jxb/vo/CourseCmtVo.java
+++ ace/ace-jxb/ace-jxb-client/src/main/java/com/huacainfo/ace/jxb/vo/CourseCmtVo.java
@@ -5,4 +5,23 @@ import com.huacainfo.ace.jxb.model.CourseCmt;
 
 public class CourseCmtVo extends CourseCmt {
     private static final long serialVersionUID = 1L;
+
+    private String nickname;
+    private String headimgurl;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getHeadimgurl() {
+        return headimgurl;
+    }
+
+    public void setHeadimgurl(String headimgurl) {
+        this.headimgurl = headimgurl;
+    }
 }
