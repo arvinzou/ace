b6be1c715bc3f3e17d98afd4ad9b31cfd8a7e3a0
249134995@qq.com <249134995@qq.com>
2018-08-23 18:49:27 +0800

    近心帮 - 课程评论查询调整

--- ace/ace-jxb/ace-jxb-client/src/main/java/com/huacainfo/ace/jxb/vo/CourseCmtQVo.java
+++ ace/ace-jxb/ace-jxb-client/src/main/java/com/huacainfo/ace/jxb/vo/CourseCmtQVo.java
@@ -6,8 +6,26 @@ import com.huacainfo.ace.jxb.model.CourseCmt;
 public class CourseCmtQVo extends CourseCmt {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 评论人名称
+     */
     private String nickname;
+    /**
+     * 课程名称
+     */
     private String courseName;
+    /**
+     * 咨询师ID
+     */
+    private String counselorId;
+
+    public String getCounselorId() {
+        return counselorId;
+    }
+
+    public void setCounselorId(String counselorId) {
+        this.counselorId = counselorId;
+    }
 
     public String getCourseName() {
         return courseName;
