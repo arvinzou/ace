f11bc230fffe604a7eec1d7d383b198f92cb2376
huacai003 <249134995@qq.com>
2018-08-24 14:59:32 +0800

    近兴帮后台

--- ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/order/test/js/testIndex.js
+++ ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/order/test/js/testIndex.js
@@ -55,7 +55,7 @@ function searching() {
 }
 
 function cancelSearch() {
-    $('#search_input').val('大家都在测亲子关系');
+    $('#search_input').val('输入标题进行搜索');
     $('.search .notice .iconfont').html('&#xe702;');
     $('.search_list').hide();
     $('#testListss').empty();
