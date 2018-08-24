fc376326710cb6140f2c2b8aea7f913cf63cfaa5
huacai003 <249134995@qq.com>
2018-08-24 14:29:34 +0800

    近兴帮后台

--- ace/ace-jxb/ace-jxb-web/src/main/webapp/content/service/counselorLevel/js/counselorLevel.js
+++ ace/ace-jxb/ace-jxb-web/src/main/webapp/content/service/counselorLevel/js/counselorLevel.js
@@ -1,4 +1,45 @@
 window.onload = function () {
-
+    initpage()
 
 };
+
+function initpage() {
+    $.jqPaginator('#pagination1', {
+        totalCounts: 20,
+        pageSize: 20,
+        visiblePages: 10,
+        currentPage: 1,
+        prev: '<li class="prev"><a href="javascript:;">上一页</a></li>',
+        next: '<li class="next"><a href="javascript:;">下一页</a></li>',
+        page: '<li class="page"><a href="javascript:;">{{page}}</a></li>',
+        onPageChange: function (num, type) {
+            getChecklist(num, type);
+        }
+    });
+}
+
+
+function getChecklist(num, type) {
+    var url = "/jxb/baseOrder/findBaseOrderListSecond";
+    var data = {
+        category: category,
+        page: num,
+        limit: 20
+    }
+    $.getJSON(url, data, function (result) {
+        if (result.status == 0) {
+            if (type == "init") {
+                $('#pagination1').jqPaginator('option', {
+                    totalCounts: result.data.total,
+                });
+            }
+            var navitem = document.getElementById('temp_orderList').innerHTML;
+            var html = juicer(navitem, {
+                orderCategory: category,
+                data: result.data.list,
+            });
+            $("#orderList").html(html);
+
+        }
+    })
+}
