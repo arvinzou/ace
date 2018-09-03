40b7f1b841b900434fefe88793fbb816a3932dd0
249134995@qq.com <249134995@qq.com>
2018-09-03 15:19:30 +0800

    近心帮-修改订单时间不能在当前时间之前--罗灿

--- ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/order/js/act.js
+++ ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/order/js/act.js
@@ -59,7 +59,7 @@ window.onload = function(){
     calendar.init({
         'trigger': '#start_date', //标签id
         'type': 'datetime', //date 调出日期选择 datetime 调出日期时间选择 time 调出时间选择 ym 调出年月选择,
-        'minDate': (new Date().getFullYear()-3) + '-' + 1 + '-' + 1, //最小日期
+        'minDate': (new Date().getFullYear()) + '-' + (new Date().getMonth()+1) + '-' + (new Date().getDate()+1), //最小日期
         'maxDate': (new Date().getFullYear()+3) + '-' + 12 + '-' + 31 //最大日期
     });
 }
