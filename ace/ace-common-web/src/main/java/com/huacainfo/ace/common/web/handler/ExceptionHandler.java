93eb4ab7ca9cb958270de96843175256b334581f
249134995 <249134995@qq.com>
2018-09-08 20:25:27 +0800

    代码自动生成

--- ace/ace-common-web/src/main/java/com/huacainfo/ace/common/web/handler/ExceptionHandler.java
+++ ace/ace-common-web/src/main/java/com/huacainfo/ace/common/web/handler/ExceptionHandler.java
@@ -5,6 +5,9 @@ import com.huacainfo.ace.common.exception.CustomException;
 import com.huacainfo.ace.common.kafka.KafkaProducerService;
 import com.huacainfo.ace.common.result.MessageResponse;
 import com.huacainfo.ace.common.tools.CommonUtils;
+import es.moki.ratelimitj.core.limiter.request.RequestLimitRule;
+import es.moki.ratelimitj.core.limiter.request.RequestRateLimiter;
+import es.moki.ratelimitj.inmemory.request.InMemorySlidingWindowRequestRateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +19,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class ExceptionHandler implements HandlerExceptionResolver {
 	private Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
@@ -31,6 +37,9 @@ public class ExceptionHandler implements HandlerExceptionResolver {
 
 	@Value("#{config[email]}")
 	private String email;
+
+	private Set<RequestLimitRule> rules = Collections.singleton(RequestLimitRule.of(5, TimeUnit.MINUTES,3));
+	private RequestRateLimiter limiter = new InMemorySlidingWindowRequestRateLimiter(rules);
 
 	@Override
 	public ModelAndView resolveException(HttpServletRequest request,
@@ -48,15 +57,19 @@ public class ExceptionHandler implements HandlerExceptionResolver {
 		if(exception instanceof CustomException){
 			messageResponse = new MessageResponse(1, "内部错误",((CustomException) exception).getMsg());
 		}
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("service", "cmccBackendService");
-		data.put("mobile", mobile);
-		data.put("email", email);
-		data.put("title","系统发生了异常，详细信息将通过邮件的方式发送给管理员的邮箱请查收");
-		data.put("content",messageResponse.getErrorMessage());
-		logger.info("========================开始发送异常消息===============================");
-		this.kafkaProducerService.sendMsg("topic.sys.msg", data);
-		logger.info("========================完成发送异常消息===============================");
+		boolean reachLimit = limiter.overLimitWhenIncremented("dealException");
+		if(!reachLimit){
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("service", "cmccBackendService");
+			data.put("mobile", mobile);
+			data.put("email", email);
+			data.put("title","系统发生了异常，详细信息将通过邮件的方式发送到管理员的邮箱，请查收。");
+			data.put("content",messageResponse.getErrorMessage());
+			logger.info("========================开始发送异常消息===============================");
+			this.kafkaProducerService.sendMsg("topic.sys.msg", data);
+			logger.info("========================完成发送异常消息===============================");
+		}
+
 		String rString = JSONObject.toJSONString(messageResponse);
 		outJsonString(response, rString);
 	}
