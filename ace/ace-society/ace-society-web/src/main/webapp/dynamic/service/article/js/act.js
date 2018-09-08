0cdfbf3181a0750528d8df347ee2b35a92873b7e
249134995 <249134995@qq.com>
2018-09-08 09:24:36 +0800

    代码自动生成

---
+++ ace/ace-society/ace-society-web/src/main/webapp/dynamic/service/article/js/act.js
@@ -0,0 +1,177 @@
+var loading = {};
+var params = {limit: 5};
+window.onload = function (){
+    initPage();
+    initEvents();
+}
+function App() {
+    console.log("=============================App Start==============================");
+    loadCustom();
+}
+/*加载资源*/
+function loadCustom() {
+    var urls = [];
+    urls.push({path: contextPath, url: '/content/common/js/jqPaginator.js', type: 'js'});
+    for (var i = 0; i < urls.length; i++) {
+        loader(urls[i]);
+    }
+}
+/*课程初始化分页*/
+function initPage() {
+    $.jqPaginator('#pagination1', {
+        totalCounts: 0,
+        pageSize: params.limit,
+        visiblePages: 10,
+        currentPage: 1,
+        prev: '<li class="prev"><a href="javascript:;">上一页</a></li>',
+        next: '<li class="next"><a href="javascript:;">下一页</a></li>',
+        page: '<li class="page"><a href="javascript:;">{{page}}</a></li>',
+        onPageChange: function (num, type) {
+            params['start']=(num-1)*params.limit;
+            params['initType']=type;
+            getPageList();
+        }
+    });
+}
+/*课程条件查询*/
+function t_query(){
+    getPageList();
+    return false;
+}
+/*课程加载表格数据*/
+function getPageList() {
+    var url = contextPath+ "/article/findArticleList";
+    params['name']=$("input[name=keyword]").val();
+    startLoad();
+    $.getJSON(url, params, function (rst) {
+        stopLoad();
+        if (rst.status == 0) {
+            if (params.initType == "init") {
+                $('#pagination1').jqPaginator('option', {
+                    totalCounts: rst.total
+                });
+            }
+            render($("#page-list"), rst.rows, "tpl-list");
+        }
+    })
+}
+/*页面渲染*/
+function render(obj, data, tplId) {
+    var tpl = document.getElementById(tplId).innerHTML;
+    var html = juicer(tpl, {
+        data: data,
+    });
+    $(obj).html(html);
+}
+/*课程添加*/
+function add(type){
+    window.location.href = add/index.jsp;
+}
+/*课程编辑*/
+function edit(id){
+     window.location.href = edit/index.jsp?id='+id;
+}
+
+function initEvents() {
+    $('#model-audit').on('show.bs.modal', function (event) {
+        var relatedTarget = $(event.relatedTarget);
+        var id = relatedTarget.data('id');
+        var modal = $(this);
+        modal.find('.modal-body input[name=id]').val(id);
+    })
+    $('#model-audit .btn-primary').on('click', function () {
+        $('#model-audit form').submit();
+    });
+    $('#model-audit form').ajaxForm({
+            beforeSubmit: function (formData, jqForm, options) {
+                var params = {};
+                $.each(formData, function (n, obj) {
+                    params[obj.name] = obj.value;
+                });
+                $.extend(params, {
+                    time: new Date()
+                });
+                console.log(params);
+                audit(params);
+                return false;
+            }
+        });
+
+
+}
+/*课程审核*/
+function audit(params){
+    startLoad();
+    $.ajax({
+        url: contextPath + "/article/audit",
+        type:"post",
+        async:false,
+        data:params,
+        success:function(rst){
+            stopLoad();
+            $("#audit").modal('hide');
+            alert(rst.errorMessage);
+            if(rst.status == 0) {
+                getPageList();
+            }
+        },
+        error:function(){
+            stopLoad();
+            alert("对不起出错了！");
+        }
+    });
+}
+/*课程上架*/
+function online(id){
+    if(confirm("确定要上架吗？")){
+        startLoad();
+        $.ajax({
+            url: contextPath + "/article/updateStatus",
+            type:"post",
+            async:false,
+            data:{
+               id :id,
+               status:'1'
+            },
+            success:function(rst){
+                stopLoad();
+                if(rst.status == 0) {
+                  getPageList();
+                }else {
+                    alert(rst.errorMessage);
+                }
+            },
+            error:function(){
+                stopLoad();
+                alert("对不起，出错了！");
+            }
+        });
+    }
+}
+/*课程下架*/
+function outline(id){
+    if(confirm("确定要下架吗？")){
+        startLoad();
+        $.ajax({
+            url: contextPath + "/article/updateStatus",
+            type:"post",
+            async:false,
+            data:{
+               id :id,
+               status:'0'
+            },
+            success:function(rst){
+                stopLoad();
+                if(rst.status == 0) {
+                  getPageList();
+                }else {
+                    alert(rst.errorMessage);
+                }
+            },
+            error:function(){
+                stopLoad();
+                alert("对不起，出错了！");
+            }
+        });
+    }
+}
