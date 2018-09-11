356522885682da24501cbacf0e8a0fdaf6c90048
huacai003 <249134995@qq.com>
2018-09-11 17:35:39 +0800

    芙蓉街道

---
+++ ace/ace-society/ace-society-service/src/main/java/com/huacainfo/ace/society/dao/ActivityDao.java
@@ -0,0 +1,35 @@
+package com.huacainfo.ace.society.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import com.huacainfo.ace.society.model.Activity;
+import com.huacainfo.ace.society.vo.ActivityQVo;
+import com.huacainfo.ace.society.vo.ActivityVo;
+
+public interface ActivityDao {
+
+    Activity selectByPrimaryKey(String id);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Activity record);
+
+    int insertSelective(Activity record);
+
+    int updateByPrimaryKey(Activity record);
+
+    int updateByPrimaryKeySelective(Activity record);
+
+    ActivityVo selectVoByPrimaryKey(String id);
+
+    List<ActivityVo> findList(@Param("condition") ActivityQVo condition,
+                              @Param("start") int start,
+                              @Param("limit") int limit,
+                              @Param("orderBy") String orderBy);
+
+    int findCount(@Param("condition") ActivityQVo condition);
+
+    int isExit(Activity record);
+
+}
