a7f9dea03448b6e59d8687ecc8eb007cee5abb4e
249134995@qq.com <249134995@qq.com>
2018-09-11 16:54:11 +0800

    芙蓉街道-市民文明行为拍审核

---
+++ ace/ace-society/ace-society-client/src/main/java/com/huacainfo/ace/society/model/Behavior.java
@@ -0,0 +1,137 @@
+package com.huacainfo.ace.society.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Behavior implements Serializable{
+    private static final long serialVersionUID = -5107602933945422294L;
+    private String id;
+
+    private String title;
+
+    private String userId;
+
+    private String compendium;
+
+    private Date submitDate;
+
+    private String remark;
+
+    private String status;
+
+    private String createUserId;
+
+    private String createUserName;
+
+    private Date createDate;
+
+    private String lastModifyUserId;
+
+    private String lastModifyUserName;
+
+    private Date lastModifyDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getCompendium() {
+        return compendium;
+    }
+
+    public void setCompendium(String compendium) {
+        this.compendium = compendium == null ? null : compendium.trim();
+    }
+
+    public Date getSubmitDate() {
+        return submitDate;
+    }
+
+    public void setSubmitDate(Date submitDate) {
+        this.submitDate = submitDate;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId == null ? null : createUserId.trim();
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName == null ? null : createUserName.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getLastModifyUserId() {
+        return lastModifyUserId;
+    }
+
+    public void setLastModifyUserId(String lastModifyUserId) {
+        this.lastModifyUserId = lastModifyUserId == null ? null : lastModifyUserId.trim();
+    }
+
+    public String getLastModifyUserName() {
+        return lastModifyUserName;
+    }
+
+    public void setLastModifyUserName(String lastModifyUserName) {
+        this.lastModifyUserName = lastModifyUserName == null ? null : lastModifyUserName.trim();
+    }
+
+    public Date getLastModifyDate() {
+        return lastModifyDate;
+    }
+
+    public void setLastModifyDate(Date lastModifyDate) {
+        this.lastModifyDate = lastModifyDate;
+    }
+}
---
+++ ace/ace-society/ace-society-client/src/main/java/com/huacainfo/ace/society/model/BehaviorAnnex.java
@@ -0,0 +1,148 @@
+package com.huacainfo.ace.society.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class BehaviorAnnex implements Serializable {
+
+    private static final long serialVersionUID = -5454882401691135125L;
+    private String id;
+
+    private String behaviorId;
+
+    private String fileType;
+
+    private String fileName;
+
+    private String fileSize;
+
+    private String fileUrl;
+
+    private String remark;
+
+    private String status;
+
+    private String createUserId;
+
+    private String createUserName;
+
+    private Date createDate;
+
+    private String lastModifyUserId;
+
+    private String lastModifyUserName;
+
+    private Date lastModifyDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getBehaviorId() {
+        return behaviorId;
+    }
+
+    public void setBehaviorId(String behaviorId) {
+        this.behaviorId = behaviorId == null ? null : behaviorId.trim();
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType == null ? null : fileType.trim();
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName == null ? null : fileName.trim();
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize == null ? null : fileSize.trim();
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl == null ? null : fileUrl.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId == null ? null : createUserId.trim();
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName == null ? null : createUserName.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getLastModifyUserId() {
+        return lastModifyUserId;
+    }
+
+    public void setLastModifyUserId(String lastModifyUserId) {
+        this.lastModifyUserId = lastModifyUserId == null ? null : lastModifyUserId.trim();
+    }
+
+    public String getLastModifyUserName() {
+        return lastModifyUserName;
+    }
+
+    public void setLastModifyUserName(String lastModifyUserName) {
+        this.lastModifyUserName = lastModifyUserName == null ? null : lastModifyUserName.trim();
+    }
+
+    public Date getLastModifyDate() {
+        return lastModifyDate;
+    }
+
+    public void setLastModifyDate(Date lastModifyDate) {
+        this.lastModifyDate = lastModifyDate;
+    }
+}
