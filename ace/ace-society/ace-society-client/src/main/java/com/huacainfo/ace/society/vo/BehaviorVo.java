283f9f2143da3ee3d17411c23cc60cc1bd4be933
249134995@qq.com <249134995@qq.com>
2018-09-12 15:09:06 +0800

    近心帮文明行为随手拍管理

--- ace/ace-society/ace-society-client/src/main/java/com/huacainfo/ace/society/vo/BehaviorVo.java
+++ ace/ace-society/ace-society-client/src/main/java/com/huacainfo/ace/society/vo/BehaviorVo.java
@@ -1,48 +1,21 @@
 package com.huacainfo.ace.society.vo;
 
 import com.huacainfo.ace.society.model.Behavior;
+import com.huacainfo.ace.society.model.BehaviorAnnex;
+
+import java.util.List;
 
 
 public class BehaviorVo extends Behavior {
     private static final long serialVersionUID = 1L;
 
-    private String fileType;
+    private List<BehaviorAnnexVo> behaviorAnnexList;
 
-    private String fileName;
-
-    private String fileSize;
-
-    private String fileUrl;
-
-    public String getFileType() {
-        return fileType;
+    public List<BehaviorAnnexVo> getBehaviorAnnexList() {
+        return behaviorAnnexList;
     }
 
-    public void setFileType(String fileType) {
-        this.fileType = fileType;
-    }
-
-    public String getFileSize() {
-        return fileSize;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public void setFileSize(String fileSize) {
-        this.fileSize = fileSize;
-    }
-
-    public String getFileUrl() {
-        return fileUrl;
-    }
-
-    public void setFileUrl(String fileUrl) {
-        this.fileUrl = fileUrl;
+    public void setBehaviorAnnexList(List<BehaviorAnnexVo> behaviorAnnexList) {
+        this.behaviorAnnexList = behaviorAnnexList;
     }
 }
