c933fd0ca403fde5c5fb53f4cd326d06ef23aca7
249134995 <249134995@qq.com>
2018-09-12 11:18:21 +0800

    解决page用户404问题

---
+++ ace/ace-jxb/ace-jxb-web/src/main/webapp/content/index/act.js
@@ -0,0 +1,24 @@
+function App(){
+	$.ajax({
+		type : "post",
+		url : contextPath + '/anslysis/query.do',
+		data:{reportId:"portal"},
+		success : function(rst) {
+		    var data={};
+			$.each(rst.value, function(i,o) {
+				data[o.id]=o.value;
+			});
+			console.log(data);
+			render($(".page-content-inner"), data, "tpl-portal")
+		}
+	});
+}
+
+/*页面渲染*/
+function render(obj, data, tplId) {
+    var tpl = document.getElementById(tplId).innerHTML;
+    var html = juicer(tpl, {
+        data: data,
+    });
+    $(obj).html(html);
+}
