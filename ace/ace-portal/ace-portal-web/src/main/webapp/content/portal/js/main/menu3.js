da441108635494536fa1245ec0635fed02cb7185
249134995 <249134995@qq.com>
2018-09-13 17:50:08 +0800

    解决page用户404问题

--- ace/ace-portal/ace-portal-web/src/main/webapp/content/portal/js/main/menu3.js
+++ ace/ace-portal/ace-portal-web/src/main/webapp/content/portal/js/main/menu3.js
@@ -146,10 +146,10 @@ var buildMenu = function(menus) {
 	var activeNode=$("#menu .active");
 	 console.log(activeNode);
          if(urlParams.id){
-            $(activeNode).parent().css('display','none');
+            //$(activeNode).parent().css('display','block');
              $(activeNode).parent().parent().addClass("active");
 
-             $(activeNode).parent().parent().parent().css('display','block');
+             //$(activeNode).parent().parent().parent().css('display','block');
              $(activeNode).parent().parent().parent().parent().addClass("active");
 
              var title=$(activeNode).find("a small").html();
