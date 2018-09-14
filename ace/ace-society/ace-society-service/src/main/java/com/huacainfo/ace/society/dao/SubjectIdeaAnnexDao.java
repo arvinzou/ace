1cbfdfa847b6b5db07abd46e32e335952f79a0e2
249134995@qq.com <249134995@qq.com>
2018-09-14 14:32:34 +0800

    芙蓉街道-罗灿

---
+++ ace/ace-society/ace-society-service/src/main/java/com/huacainfo/ace/society/dao/SubjectIdeaAnnexDao.java
@@ -0,0 +1,35 @@
+package com.huacainfo.ace.society.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import com.huacainfo.ace.society.model.SubjectIdeaAnnex;
+import com.huacainfo.ace.society.vo.SubjectIdeaAnnexQVo;
+import com.huacainfo.ace.society.vo.SubjectIdeaAnnexVo;
+
+public interface SubjectIdeaAnnexDao {
+
+SubjectIdeaAnnex selectByPrimaryKey(String id);
+
+int deleteByPrimaryKey(String id);
+
+int insert(SubjectIdeaAnnex record);
+
+int insertSelective(SubjectIdeaAnnex record);
+
+int updateByPrimaryKey(SubjectIdeaAnnex record);
+
+int updateByPrimaryKeySelective(SubjectIdeaAnnex record);
+
+SubjectIdeaAnnexVo selectVoByPrimaryKey(String id);
+
+List
+<SubjectIdeaAnnexVo> findList(@Param("condition") SubjectIdeaAnnexQVo condition,
+    @Param("start") int start,
+    @Param("limit") int limit,
+    @Param("orderBy") String orderBy);
+
+    int findCount(@Param("condition") SubjectIdeaAnnexQVo condition);
+
+    int isExit(SubjectIdeaAnnex record);
+
+    }
