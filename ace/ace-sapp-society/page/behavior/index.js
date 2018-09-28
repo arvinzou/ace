c65a7871fc84995820f9c2ff437a12dd5a67676d
249134995@qq.com <249134995@qq.com>
2018-09-28 16:19:36 +0800

    芙蓉街道

--- ace/ace-sapp-society/page/behavior/index.js
+++ ace/ace-sapp-society/page/behavior/index.js
@@ -24,7 +24,11 @@ Page({
             currentTab: e.detail.current,
         })
     },
-
+    releaseBehavior: function(){
+        wx.navigateTo({
+            url: '../releaseBehavior/index'
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
