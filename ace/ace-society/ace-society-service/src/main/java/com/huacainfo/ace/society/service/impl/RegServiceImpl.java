40e71d1caef8eb7468af72b344e6c6bda07f9304
249134995@qq.com <249134995@qq.com>
2018-10-11 17:52:21 +0800

    芙蓉街道 - 注册信息调整

--- ace/ace-society/ace-society-service/src/main/java/com/huacainfo/ace/society/service/impl/RegServiceImpl.java
+++ ace/ace-society/ace-society-service/src/main/java/com/huacainfo/ace/society/service/impl/RegServiceImpl.java
@@ -4,7 +4,7 @@ import com.huacainfo.ace.common.constant.ResultCode;
 import com.huacainfo.ace.common.exception.CustomException;
 import com.huacainfo.ace.common.model.BaseModel;
 import com.huacainfo.ace.common.model.UserProp;
-import com.huacainfo.ace.common.model.Userinfo;
+import com.huacainfo.ace.common.model.WxUser;
 import com.huacainfo.ace.common.result.MessageResponse;
 import com.huacainfo.ace.common.result.ResultResponse;
 import com.huacainfo.ace.common.tools.CommonBeanUtils;
@@ -78,13 +78,13 @@ public class RegServiceImpl implements RegService {
     /**
      * 统一注册
      *
-     * @param regType  注册类型 1 - 个人/党员 ，2 - 社会/党组织
-     * @param regInfo  注册信息
-     * @param userinfo 微信识别信息
+     * @param regType 注册类型 1 - 个人/党员 ，2 - 社会/党组织
+     * @param regInfo 注册信息
+     * @param wxUser  微信识别信息
      * @return
      */
     @Override
-    public ResultResponse register(String regType, BaseModel regInfo, Userinfo userinfo) throws Exception {
+    public ResultResponse register(String regType, BaseModel regInfo, WxUser wxUser) throws Exception {
         String mobile;
         String nickname;
         PersonInfo personInfo = null;
@@ -93,14 +93,14 @@ public class RegServiceImpl implements RegService {
         //个人/党员
         if (RegType.PERSON.equals(regType)) {
             personInfo = (PersonInfo) regInfo;
-            personInfo.setId(userinfo.getUnionid());
+            personInfo.setId(wxUser.getUnionId());
             mobile = personInfo.getMobilePhone();
             nickname = personInfo.getRealName();
         }
         //社会/党组织
         else if (RegType.ORG.equals(regType)) {
             orgInfo = (SocietyOrgInfo) regInfo;
-            orgInfo.setId(userinfo.getUnionid());
+            orgInfo.setId(wxUser.getUnionId());
             mobile = orgInfo.getContactPhone();
             nickname = orgInfo.getOrgName();
 
@@ -111,8 +111,8 @@ public class RegServiceImpl implements RegService {
         Reg reg = new Reg();
         reg.setNickname(nickname);
         reg.setMobile(mobile);
-        reg.setUnionId(userinfo.getUnionid());
-        reg.setSex(userinfo.getSex());
+        reg.setUnionId(wxUser.getUnionId());
+        reg.setSex(wxUser.getGender());
         ResultResponse regRs = new ResultResponse(insertReg(reg, regType));
         if (regRs.getStatus() == ResultCode.FAIL) {
 //            throw new CustomException(regRs.getInfo());
