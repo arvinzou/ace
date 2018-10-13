04e1e8fb57bdb4aa79c2ec0c3c441aef67808337
249134995 <249134995@qq.com>
2018-10-13 19:25:01 +0800

    解决隔离微信鉴权问题

--- ace/ace-portal/ace-portal-web/src/main/webapp/content/service/queueCmccWait/model.js
+++ ace/ace-portal/ace-portal-web/src/main/webapp/content/service/queueCmccWait/model.js
@@ -1,21 +1,9 @@
-var _colNames = [ '编号','手机号','姓名','短信内容','入队时间' ];
+var _colNames = ['手机号','短信内容','入队时间' ];
 var _colModel = function() {
-	return [ {
-		name : 'queueId',
-		index : 'queueId',
-		width : 200,
-		sortable : false,
-		editable : false
-	},{
+	return [{
 		name : 'tel',
 		index : 'tel',
 		width : 100,
-		sortable : false,
-		editable : false
-	},{
-		name : 'name',
-		index : 'name',
-		width : 60,
 		sortable : false,
 		editable : false
 	},{
