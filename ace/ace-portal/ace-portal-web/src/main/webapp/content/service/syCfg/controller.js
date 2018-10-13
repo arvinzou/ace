1d1db094e24ff59bc7f07faa2a66721fc649b35f
249134995 <249134995@qq.com>
2018-10-13 11:59:57 +0800

    解决隔离微信鉴权问题

--- ace/ace-portal/ace-portal-web/src/main/webapp/content/service/syCfg/controller.js
+++ ace/ace-portal/ace-portal-web/src/main/webapp/content/service/syCfg/controller.js
@@ -1,18 +1,18 @@
 jQuery(function($) {
 	$('#btn-search').on('click', function() {
 		$('#fm-search').ajaxForm({
-			beforeSubmit : function(formData, jqForm, options) {
+			beforeSubmit: function(formData, jqForm, options) {
 				var params = {};
 				$.each(formData, function(n, obj) {
 					params[obj.name] = obj.value;
 				});
 				$.extend(params, {
-					time : new Date()
+					time: new Date()
 				});
 				// console.log(params);
 				jQuery(cfg.grid_selector).jqGrid('setGridParam', {
-					page : 1,
-					postData : params
+					page: 1,
+					postData: params
 				}).trigger("reloadGrid");
 				return false;
 			}
@@ -20,61 +20,53 @@ jQuery(function($) {
 	});
 
 	$('#btn-view-add').on(
-			'click',
-			function() {
-				jQuery(cfg.grid_selector).jqGrid(
-						'editGridRow',
-						'new',
-						{
-							closeAfterAdd : true,
-							recreateForm : true,
-							viewPagerButtons : false,
-							beforeShowForm : function(e) {
-								var form = $(e[0]);
-								form.closest('.ui-jqdialog').find(
-										'.ui-jqdialog-titlebar').wrapInner(
-										'<div class="widget-header" />')
+		'click',
+		function() {
+			jQuery(cfg.grid_selector).jqGrid(
+				'editGridRow',
+				'new', {
+					closeAfterAdd: true,
+					recreateForm: true,
+					viewPagerButtons: false,
+					beforeShowForm: function(e) {
+						var form = $(e[0]);
+						form.closest('.ui-jqdialog').find(
+							'.ui-jqdialog-titlebar').wrapInner(
+							'<div class="widget-header" />')
 
-							}
-						})
-			});
+					}
+				})
+		});
 });
-
-
-
-function edit(rowid){
-    console.log(rowid);
+function edit(rowid) {
+	console.log(rowid);
 	jQuery(cfg.grid_selector).jqGrid(
-						'editGridRow',
-						rowid,
-						{
-							closeAfterAdd : true,
-							recreateForm : true,
-							viewPagerButtons : true,
-							beforeShowForm : function(e) {
-								var form = $(e[0]);
-								form.closest('.ui-jqdialog').find(
-										'.ui-jqdialog-titlebar').wrapInner(
-										'<div class="widget-header" />')
+		'editGridRow',
+		rowid, {
+			closeAfterAdd: true,
+			recreateForm: true,
+			viewPagerButtons: true,
+			beforeShowForm: function(e) {
+				var form = $(e[0]);
+				form.closest('.ui-jqdialog').find(
+					'.ui-jqdialog-titlebar').wrapInner(
+					'<div class="widget-header" />')
 
-							}
-						});
+			}
+		});
 }
-var show=false;
-function del(rowid){
-    console.log(rowid);
+var show = false;
+function del(rowid) {
+	console.log(rowid);
 	jQuery(cfg.grid_selector).jqGrid(
-    						'delGridRow',
-    						rowid,
-    						{
-    							beforeShowForm : function(e) {
-    								var form = $(e[0]);
-    								if(!show){
-    								    form.closest('.ui-jqdialog').find('.ui-jqdialog-titlebar').wrapInner('<div class="widget-header" />');
-    								}
-
-    								show=true;
-
-    							}
-    						});
+		'delGridRow',
+		rowid, {
+			beforeShowForm: function(e) {
+				var form = $(e[0]);
+				if (!show) {
+					form.closest('.ui-jqdialog').find('.ui-jqdialog-titlebar').wrapInner('<div class="widget-header" />');
+				}
+				show = true;
+			}
+		});
 }
--- ace/ace-portal/ace-portal-web/src/main/webapp/content/service/taskCmcc/model.js
+++ ace/ace-portal/ace-portal-web/src/main/webapp/content/service/taskCmcc/model.js
@@ -1,82 +1,80 @@
-var _colNames = [ '编号','发送状态', '任务名称', '短信内容', '创建时间', '发送人' ];
+var _colNames = ['编号', '发送状态', '任务名称', '短信内容', '创建时间', '发送人', '操作'];
 var _colModel = function() {
-	return [ {
-		name : 'taskId',
-		index : 'taskId',
-		width : 100,
-		sortable : false,
-		editable : true,
-		editoptions : {
-			readonly : true
+	return [{
+		name: 'taskId',
+		index: 'taskId',
+		width: 100,
+		sortable: false,
+		editable: true,
+		editoptions: {
+			readonly: true
 		}
-	},{
-		name : 'status',
-		index : 'status',
-		width : 100,
-		sortable : false,
-		editable : false,
-		renderer : function(value) {
+	}, {
+		name: 'status',
+		index: 'status',
+		width: 60,
+		sortable: false,
+		editable: false,
+		renderer: function(value) {
 			var rst = "";
 			switch (value) {
-			case '1':
-				rst = "YES";
-				break;
-			case '0':
-				rst = "NO";
-				break;
-			default:
-				rst = "N/A";
+				case '1':
+					rst = "YES";
+					break;
+				case '0':
+					rst = "NO";
+					break;
+				default:
+					rst = "N/A";
 			}
 			return rst;
 		}
 	}, {
-		name : 'taskName',
-		index : 'taskName',
-		width : 150,
-		editable : true,
-		editoptions : {
-			size : "20",
-			maxlength : "30"
+		name: 'taskName',
+		index: 'taskName',
+		width: 150,
+		editable: true,
+		editoptions: {
+			size: "20",
+			maxlength: "30"
 		}
 	}, {
-		name : 'msg',
-		index : 'msg',
-		width : 400,
-		editable : true,
-		editoptions : {
-			size : "20",
-			maxlength : "30"
+		name: 'msg',
+		index: 'msg',
+		width: 300,
+		editable: true,
+		editoptions: {
+			size: "20",
+			maxlength: "30"
 		}
 	}, {
-		name : 'createTime',
-		width : 150,
-		sortable : true,
-		editable : false
+		name: 'createTime',
+		width: 150,
+		sortable: true,
+		editable: false
 	}, {
-		name : 'name',
-		index : 'name',
-		width : 80,
-		editable : true,
-		editoptions : {
-			size : "20",
-			maxlength : "30"
+		name: 'name',
+		index: 'name',
+		width: 80,
+		editable: true,
+		editoptions: {
+			size: "20",
+			maxlength: "30"
 		}
-	} ];
-}
-function aceSwitch(cellvalue, options, cell) {
-	console.log('aceSwitch');
-	setTimeout(function() {
-		$(cell).find('input[type=checkbox]').addClass(
-				'ace ace-switch ace-switch-5').after(
-				'<span class="lbl"></span>');
-	}, 0);
-}
-// enable datepicker
-function pickDate(cellvalue, options, cell) {
-	setTimeout(function() {
-		$(cell).find('input[type=text]').datepicker({
-			format : 'yyyy-mm-dd',
-			autoclose : true
-		});
-	}, 0);
+	}, {
+		name: 'opt',
+		sortable: false,
+		width: 100,
+		renderer: function(value, cur) {
+			var rowid = $.jgrid.getAccessor(cur, cfg.dataId);
+			var opt = [];
+			if (authorConfig.hasOwnProperty(cfg.grid_delete_data_url)) {
+				opt.push('<a href="javascript:del(\'' + rowid + '\')">删除</a>  ');
+			}
+			opt.push('<a href="javascript:preview(\'' + rowid + '\')">查看</a>  ');
+			opt.push('<a href="javascript:resend(\'' + rowid + '\')">重发送</a>');
+
+			return opt.join('');
+		}
+	}];
 }
