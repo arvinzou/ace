395f9628faa9f532fdb833c84c086a746db33abe
249134995@qq.com <249134995@qq.com>
2018-10-16 14:34:52 +0800

    芙蓉街道修改-罗灿

--- ace/ace-sapp-society/page/me/index.js
+++ ace/ace-sapp-society/page/me/index.js
@@ -1,19 +1,45 @@
+var util = require("../../util/util.js");
+var cfg = require("../../config.js");
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+      
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+      var that = this;
+      console.log("util.is_login()============================================="+util.is_login());
+      if (!util.is_login()) {
+          wx.navigateTo({ url: "../userinfo/index?url=../me/index" });
+      }else{
+          that.setData({
+              userinfo: wx.getStorageSync('userinfo')
+          });
+        
+      }
+      
   },
+  initUserData: function () {
+        util.request(cfg.findUserInfo, {},
+            function (ret) {
+                if (ret.status == 0) {
+                    return true;
+                } else {
+                    if (ret.info == '用户尚未注册') {
+                        return false;
+                    }
+                }
 
+            }
+        );
+    },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -39,7 +65,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+      var that = this;
+      if (wx.getStorageSync('userinfo')){
+          if(!that.initUserData()){
+              wx.navigateTo({ url: "../regist/index" });
+          }
+      }
   },
 
   /**
