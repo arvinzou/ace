c1901d4c399a7c106d31885d1093e491dcd56bd6
249134995@qq.com <249134995@qq.com>
2018-10-23 16:38:07 +0800

    芙蓉街道-罗灿

--- ace/ace-sapp-society/page/idea/index.js
+++ ace/ace-sapp-society/page/idea/index.js
@@ -37,47 +37,15 @@ initList: function(){
 },
 downloadFile: function(e){
     var that = this;
-    var fileUrl = e.currentTarget.dataset.value;
-    console.log("fileUrl====================================="+fileUrl);
-    var fileType = fileUrl.substring(fileUrl.lastIndexOf(".")+1, fileUrl.length);
-    //附件类型为图片
-    if(fileType == 'jpg' || fileType == 'jpeg' || fileType == 'png'){
-        wx.previewImage({
-            current: fileUrl, // 当前显示图片的http链接
-            urls: [fileUrl] // 需要预览的图片http链接列表
-        })
+    var fileObjList = e.currentTarget.dataset.value;
+    var fileList = [];
+    for(var i=0; i<fileObjList.length; i++){
+        fileList.push(fileObjList[i].fileUrl);
     }
-    //附件类型为文本
-    if(fileType == 'doc' || fileType == 'docx' || fileType == 'ppt' || fileType == 'pptx' || fileType == 'xls' || fileType == 'xlsx'){
-        wx.downloadFile({
-            // 示例 url，并非真实存在
-            url: fileUrl,
-            success: function (res) {
-                const filePath = res.tempFilePath
-                wx.openDocument({
-                    filePath: filePath,
-                    success: function (res) {
-                        console.log('打开文档成功');
-                    },
-                    fail: function(res){
-                        console.log('打开文档失败================='+res);
-                    }
-                })
-            }
-        })
-    }
-    //附件类型为视频
-    if(fileType == 'mp4'){
-        wx.saveVideoToPhotosAlbum({
-            filePath: fileUrl,
-            success(res) {
-                console.log(res.errMsg)
-            },
-            fail(res){
-                console.log("保存视频失败！"+res.errMsg);
-            }
-        })
-    }
+    wx.previewImage({
+        current: fileList[0], // 当前显示图片的http链接
+        urls: fileList// 需要预览的图片http链接列表
+    });
 },
 /**
  * 发布我有点子
