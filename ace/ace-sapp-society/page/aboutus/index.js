e9f306a55129bdd7b26677b58b42770df10e8d11
249134995@qq.com <249134995@qq.com>
2018-10-24 11:38:47 +0800

    芙蓉街道-罗灿

---
+++ ace/ace-sapp-society/page/aboutus/index.js
@@ -0,0 +1,88 @@
+var util = require("../../util/util.js");
+var cfg = require("../../config.js");
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      longitude: null,
+      latitude: null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+      var that = this;
+      this.mapCtx = wx.createMapContext('myMap');//获取地图对象同canvas相似，获取后才能调用相应的方法
+      this.mapCtx.moveToLocation()//将当前位置移动到地图中心
+  },
+    map: function () {
+        /*wx.getLocation({
+            type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+            success: function (res) {
+                var latitude = res.latitude
+                var longitude = res.longitude
+                wx.openLocation({
+                    latitude: latitude,
+                    longitude: longitude,
+                    scale: 28
+                })
+            }
+        })*/
+        wx.openLocation({
+            latitude: 29.0327722622,
+            longitude: 111.6520303488,
+            scale:28
+        })
+    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
