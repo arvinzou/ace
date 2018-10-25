4e01f065930e7ecc51fbc50554a6d5855fe48798
huacai003 <249134995@qq.com>
2018-10-25 11:42:22 +0800

    芙蓉街道

--- ace/ace-sapp-society/config.js
+++ ace/ace-sapp-society/config.js
@@ -1,17 +1,18 @@
-var server = "https://zhsh.wuling.gov.cn";
-var serverfile = "https://zhsh.wuling.gov.cn/";
-var rtmpserver = "rtmp://zhsh.wuling.gov.cn/live/";
-var hlsserver = "http://zhsh.wuling.gov.cn:9999/hls/";
-var websocketurl = 'zhsh.wuling.gov.cn';
+
+var server = "https://zx.huacainfo.com";
+var serverfile = "https://zx.huacainfo.com/";
+var rtmpserver ="rtmp://zx.huacainfo.com/live/";
+var hlsserver ="http://139.224.0.227:9999/hls/";
+var websocketurl= 'zx.huacainfo.com';
 var config = {
     rtmpserver,
     websocketurl,
     server,
     serverfile,
     hlsserver,
-    appid: 'wx7e6a369d8fa12b12',
-    appsecret: 'eac1cdaebc47d72133d0872a5900f344',
-    companyId: '0002',
+    appid: 'wx70ba7c5dca85e4da',
+    appsecret: '96214b4a01153935995cc027ddf26d75',
+    companyId: '00010001',
     loginUrl: `${server}/portal/www/authority.do`,
     checkImageUrl: `${server}/society/www/captcha/image.do`,
     uploadUrl: `${server}/portal/www/upload.do`,
@@ -26,34 +27,42 @@ var config = {
     findUserInfo: `${server}/society/www/reg/findByUserId`,
     createOrder: `${server}/society/www/order/create`,
     behaviorList: `${server}/society/www/behavior/findList`,
-    findActivityReport:`${server}/society/www/activity/selectActivityReportByPrimaryKey`,
-    findSocietyOrgInfos:`${server}/society/www/activity/findSocietyOrgInfoList`,
-    findActivitys:`${server}/society/www/activity/findActivityList`,
-    findActivity:`${server}/society/www/activity/selectActivityByPrimaryKey`,
-    publicActivityReports:`${server}/society/www/activity/findPublicActivityReportList`,
-    applyStatus:`${server}/society/www/activity/getApplyStatus`,
-    insertActivity:`${server}/society/www/activity/insertActivity`,
-    participants:`${server}/society/www/activity/findActivityParticipants`,
-    selectActivityReport:`${server}/society/www/activity/selectActivityReportByActivityId`,
-    updateActivityReport:`${server}/society/www/activity/updateActivityReport`,
+    findActivityReport: `${server}/society/www/activity/selectActivityReportByPrimaryKey`,
+    findSocietyOrgInfos: `${server}/society/www/activity/findSocietyOrgInfoList`,
+    findActivitys: `${server}/society/www/activity/findPublicActivityList`,
+    findActivity: `${server}/society/www/activity/selectActivityByPrimaryKey`,
+    publicActivityReports: `${server}/society/www/activity/findPublicActivityReportList`,
+    applyStatus: `${server}/society/www/activity/getApplyStatus`,
+    insertActivity: `${server}/society/www/activity/insertActivity`,
+    participants: `${server}/society/www/activity/findActivityParticipants`,
+    selectActivityReport: `${server}/society/www/activity/selectActivityReportByActivityId`,
+    updateActivityReport: `${server}/society/www/activity/updateActivityReport`,
     pointRank: `${server}/society/www/user/pointsRank`,
     findOrderList: `${server}/society/www/user/findOrderList`,
     findPointsRecord: `${server}/society/www/user/findPointsRecord`,
     findActivitying: `${server}/society/www/activity/findActivitying`,
     findPublicActivityReportList: `${server}/society/www/activity/findPublicActivityReportList`,
-        getLiveMsgList: `${server}/live/www/live/getLiveMsgList`,
-      insertLiveRpt: `${server}/live/liveRpt/www/insertLiveRpt`,
-        getLiveRptList: `${server}/live/www/live/getLiveRptList`,
-        insertLive: `${server}/live/live/www/insertLive`,
-        updateLive: `${server}/live/live/www/updateLive`,
-        selectLiveByPrimaryKey: `${server}/live/live/www/selectLiveByPrimaryKey`,
-      deleteLiveByLiveId: `${server}/live/live/www/deleteLiveByLiveId`,
-        findLiveList: `${server}/live/live/www/findLiveList`,
-      updateAuditStatus: `${server}/live/live/www/updateAuditStatus`,
-      insertLiveCmt: `${server}/live/liveCmt/www/insertLiveCmt`,
-      addLike: `${server}/live/www/live/addLike`,
-      getliveListByCorpId: `${server}/live/live/www/getliveListByCorpId`,
-      loadLive: `${server}/live/live/www/loadLive`
+
+    adminActivityList: `${server}/society/www/activity/findAdminSelfActivityList`,
+    userActivityList: `${server}/society/www/activity/findUserSelfActivityList`,
+    activitySign: `${server}/society/www/activity/activitySign`,
+
+
+    getLiveMsgList: `${server}/live/www/live/getLiveMsgList`,
+    insertLiveRpt: `${server}/live/liveRpt/www/insertLiveRpt`,
+    getLiveRptList: `${server}/live/www/live/getLiveRptList`,
+    insertLive: `${server}/live/live/www/insertLive`,
+    updateLive: `${server}/live/live/www/updateLive`,
+    selectLiveByPrimaryKey: `${server}/live/live/www/selectLiveByPrimaryKey`,
+    deleteLiveByLiveId: `${server}/live/live/www/deleteLiveByLiveId`,
+    findLiveList: `${server}/live/live/www/findLiveList`,
+    updateAuditStatus: `${server}/live/live/www/updateAuditStatus`,
+    insertLiveCmt: `${server}/live/liveCmt/www/insertLiveCmt`,
+    addLike: `${server}/live/www/live/addLike`,
+    getliveListByCorpId: `${server}/live/live/www/getliveListByCorpId`,
+    loadLive: `${server}/live/live/www/loadLive`,
+    submitComment: `${server}/society/www/comment/submitComment`,
+    findComments: `${server}/society/www/comment/findList`,
+    findAdmires: `${server}/society/www/comment/findAdmireTotal`,
 };
 module.exports = config
-
