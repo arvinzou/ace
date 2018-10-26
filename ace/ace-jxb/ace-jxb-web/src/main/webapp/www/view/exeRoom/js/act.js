343da0a4e9ae1d751a8db343959c4d5fccc6959a
249134995@qq.com <249134995@qq.com>
2018-10-26 09:35:03 +0800

    近心帮优秀工作室添加课程

--- ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/exeRoom/js/act.js
+++ ace/ace-jxb/ace-jxb-web/src/main/webapp/www/view/exeRoom/js/act.js
@@ -5,6 +5,7 @@ window.onload = function() {
     console.log(primaryId);
     initData(primaryId);
     initStudioReport(primaryId);
+    initFineCourse(primaryId);
 };
 
 function initData(id){
@@ -83,10 +84,46 @@ function initStudioReport(id){
     });
 }
 
+/**
+ * 查询精品课程
+ */
+function initFineCourse(id){
+    $.ajax({
+        url: contextPath+ "/www/studio/findExcellentCourse",
+        type:"post",
+        async:false,
+        data:{
+            "studioId": id
+        },
+        success:function(result){
+            if(result.status == 0) {
+                console.log(result.data.rows);
+                var courseTemp = document.getElementById('course-tpl').innerHTML;
+                var html = juicer(courseTemp, {
+                    data: result.data.rows
+                });
+                $("#qualityCourse").append(html);
+            }else {
+                alert(result.info);
+                return;
+            }
+        },
+        error:function(){
+            alert("系统服务内部异常！");
+        }
+    });
+}
 function showMember(id){
     window.location.href = contextPath + '/www/view/workmember/index.jsp?id='+id;
 }
 
 function invate(id){
     window.location.href = contextPath + '/www/view/invate/index.jsp?id='+id;
+}
+/**
+ * 查看课程详情
+ * @param id
+ */
+function courseDetail(id){
+    window.location.href = contextPath + '/www/view/courseSummary/index.jsp?id='+id;
 }
