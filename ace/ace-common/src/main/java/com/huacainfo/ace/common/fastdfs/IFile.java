33ba09201083ec8b82148637380413316165a18f
ArvinZou <309751246@qq.com>
2018-11-08 16:48:21 +0800

    common - 增加文件上传接口

--- ace/ace-common/src/main/java/com/huacainfo/ace/common/fastdfs/IFile.java
+++ ace/ace-common/src/main/java/com/huacainfo/ace/common/fastdfs/IFile.java
@@ -4,19 +4,21 @@ import java.io.File;
 import java.io.OutputStream;
 
 public interface IFile {
-	public String saveFile(File file, String fileSaveName) throws Exception;
+    String saveFile(byte[] bytes, String fileSaveName) throws Exception;
 
-	public String saveFile(File file) throws Exception;
+    public String saveFile(File file, String fileSaveName) throws Exception;
 
-	public byte[] getFileBytes(String filePath, Long startPos, Long length)
-			throws Exception;
+    public String saveFile(File file) throws Exception;
 
-	public void outputFile(String filePath, OutputStream output)
-			throws Exception;
+    public byte[] getFileBytes(String filePath, Long startPos, Long length)
+            throws Exception;
 
-	public boolean deleteFile(String filePath) throws Exception;
+    public void outputFile(String filePath, OutputStream output)
+            throws Exception;
 
-	public long getSize(String filePath) throws Exception;
+    public boolean deleteFile(String filePath) throws Exception;
 
-	public void copy(String sourcePath, String targetPath) throws Exception;
+    public long getSize(String filePath) throws Exception;
+
+    public void copy(String sourcePath, String targetPath) throws Exception;
 }
