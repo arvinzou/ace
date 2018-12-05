9ac7b6977d6baa4472e25e53c7cb36ea98626709
ArvinZou <309751246@qq.com>
2018-12-05 10:22:48 +0800

    芙蓉街道 - 个人&党员 增加所属组织信息

--- ace/ace-society/ace-society-client/src/main/java/com/huacainfo/ace/society/vo/PersonInfoQVo.java
+++ ace/ace-society/ace-society-client/src/main/java/com/huacainfo/ace/society/vo/PersonInfoQVo.java
@@ -5,4 +5,16 @@ import com.huacainfo.ace.society.model.PersonInfo;
 
 public class PersonInfoQVo extends PersonInfo {
     private static final long serialVersionUID = 1L;
+    /**
+     * 所属组织名称
+     */
+    private String orgName;
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
 }
