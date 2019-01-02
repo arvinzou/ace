2194983e583a33b826ddc6874c3dbdf65b3c2afd
ArvinZou <309751246@qq.com>
2019-01-02 16:18:41 +0800

    codec - 旧框架代码调整

--- ace/ace-codeGenerator/src/main/java/com/huacainfo/ace/generator/partyschool/Generator.java
+++ ace/ace-codeGenerator/src/main/java/com/huacainfo/ace/generator/partyschool/Generator.java
@@ -5,18 +5,18 @@ import com.huacainfo.ace.generator.base.CodeGenerator;
 public class Generator {
 
     public static void main(String[] args) throws Exception {
-        CodeGenerator codeGenerator = new CodeGenerator("partyschool", "学员管理", "Arvin");
-        Class<?> c = com.huacainfo.ace.partyschool.model.Student.class;
+        CodeGenerator codeGenerator = new CodeGenerator("partyschool", "教职工管理", "Arvin");
+        Class<?> c = null;// com.huacainfo.ace.partyschool.model.Teacher.class;
 
         //*******************************************
         codeGenerator.init(c);
         codeGenerator.generatorJavaManager(c);
         //web形式-适用于新互联网架构
-        codeGenerator.generatorWebClient1(c);
+//        codeGenerator.generatorWebClient1(c);
 
 
         // web形式-适用于多表格，多数据的表单结构
-//        codeGenerator.generatorWebClient2(c);
+        codeGenerator.generatorWebClient2(c);
 
     }
 
