0e56b3c417add6be3eaec12f1a4a470de84a0c56
huacai003 <249134995@qq.com>
2019-01-07 09:22:48 +0800

    党校-为

---
+++ ace/ace-partyschool/ace-partyschool-client/src/main/java/com/huacainfo/ace/partyschool/model/ClassSchedule.java
@@ -0,0 +1,67 @@
+package com.huacainfo.ace.partyschool.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ClassSchedule  implements Serializable {
+    private  static final long serialVersionUID = 1L;
+    private String id;
+
+    private String classesId;
+
+    private Date courseDate;
+
+    private String courseIndex;
+
+    private String teacherId;
+
+    private String courseId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getClassesId() {
+        return classesId;
+    }
+
+    public void setClassesId(String classesId) {
+        this.classesId = classesId == null ? null : classesId.trim();
+    }
+
+    public Date getCourseDate() {
+        return courseDate;
+    }
+
+    public void setCourseDate(Date courseDate) {
+        this.courseDate = courseDate;
+    }
+
+    public String getCourseIndex() {
+        return courseIndex;
+    }
+
+    public void setCourseIndex(String courseIndex) {
+        this.courseIndex = courseIndex == null ? null : courseIndex.trim();
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId == null ? null : teacherId.trim();
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId == null ? null : courseId.trim();
+    }
+}
