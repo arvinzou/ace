5318fe47119a9479feedeebc8e5f470f2987d0ef
pf <844467110@qq.com>
2019-01-08 08:55:34 +0800

    班级文件前端接口

--- ace/ace-partyschool/ace-partyschool-client/src/main/java/com/huacainfo/ace/partyschool/service/clsFilesService.java
+++ ace/ace-partyschool/ace-partyschool-client/src/main/java/com/huacainfo/ace/partyschool/service/clsFilesService.java
@@ -3,6 +3,7 @@ package com.huacainfo.ace.partyschool.service;
 import com.huacainfo.ace.common.model.UserProp;
 import com.huacainfo.ace.common.result.MessageResponse;
 import com.huacainfo.ace.common.result.PageResult;
+import com.huacainfo.ace.common.result.ResultResponse;
 import com.huacainfo.ace.common.result.SingleResult;
 import com.huacainfo.ace.partyschool.model.Files;
 import com.huacainfo.ace.partyschool.vo.FilesVo;
@@ -28,9 +29,10 @@ public interface clsFilesService {
      * @author: Arvin
      * @version: 2019-01-04
      */
-    PageResult
-            <FilesVo> findFilesList(FilesQVo condition,
-                                    int start, int limit, String orderBy) throws Exception;
+    PageResult<FilesVo> findFilesList(FilesQVo condition, int start, int limit, String orderBy) throws Exception;
+
+    ResultResponse findFilesListVo(FilesQVo condition, int start, int limit, String orderBy, UserProp userProp) throws Exception;
+
 
     /**
      * @throws
