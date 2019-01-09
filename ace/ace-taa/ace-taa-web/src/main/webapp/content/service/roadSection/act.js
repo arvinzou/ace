eccc803a3a9166cc141b53c03bf009f2d914bbd0
249134995 <249134995@qq.com>
2019-01-09 21:31:05 +0800

    工商联统计分析页面优化

--- ace/ace-taa/ace-taa-web/src/main/webapp/content/service/roadSection/act.js
+++ ace/ace-taa/ace-taa-web/src/main/webapp/content/service/roadSection/act.js
@@ -24,9 +24,44 @@ function initPreview(id) {
                             }
                             var polygon = new qq.maps.Polyline({
                                 map: map,
+                                strokeColor: '#FF0000',
+                                //strokeColor: new qq.maps.Color(0, 0, 0, 0.5),
+                                //折线的样式
+                                strokeDashStyle: 'solid',
+                                //折线的宽度
+                                strokeWeight: 6,
+                                //折线末端线帽的样式
+                                strokeLinecap: 'round',
                                 path: path,
                                 editable: true
                             });
+                             //创建一个Marker
+                                var marker = new qq.maps.Marker({
+                                    //设置Marker的位置坐标
+                                    position: new qq.maps.LatLng(o.latitude,o.longitude),
+                                    //设置Marker被添加到Map上时的动画效果为落下
+                                    animation: qq.maps.MarkerAnimation.DOWN,
+                                    //设置Marker被添加到Map上时的动画效果为反复弹跳
+                                    //animation:qq.maps.MarkerAnimation.BOUNCE
+                                    //设置Marker被添加到Map上时的动画效果为从天而降
+                                    animation:qq.maps.MarkerAnimation.DROP,
+                                    //设置Marker被添加到Map上时的动画效果为升起
+                                    //animation:qq.maps.MarkerAnimation.UP
+                                    //设置显示Marker的地图
+                                    map: map,
+                                    //设置Marker可拖动
+                                    draggable: true,
+                                    //Marker的覆盖内容
+                                    decoration: new qq.maps.MarkerDecoration("<font style='color:#fff;font-size:10px'>标记</font>"),
+                                    //自定义Marker图标为大头针样式
+                                    icon: new qq.maps.MarkerImage("http://3gimg.qq.com/tencentMapTouch/lbs/img/nilt.png"),
+                                    //自定义Marker图标的阴影
+                                   // shadow: new qq.maps.MarkerImage("https://open.map.qq.com/doc/img/nilb.png"),
+                                    //设置Marker标题，鼠标划过Marker时显示
+                                    title: '测试',
+                                    //设置Marker的可见性，为true时可见,false时不可见
+                                    visible: true
+                                });
                         }
 
                     } else {
