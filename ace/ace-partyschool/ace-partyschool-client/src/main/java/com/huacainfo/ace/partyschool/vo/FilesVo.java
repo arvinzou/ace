1dc48e48cfe335685b5782d0abf79a74ecfa496c
pf <844467110@qq.com>
2019-01-09 15:12:06 +0800

    班级文件

--- ace/ace-partyschool/ace-partyschool-client/src/main/java/com/huacainfo/ace/partyschool/vo/FilesVo.java
+++ ace/ace-partyschool/ace-partyschool-client/src/main/java/com/huacainfo/ace/partyschool/vo/FilesVo.java
@@ -1,8 +1,29 @@
 package com.huacainfo.ace.partyschool.vo;
 
 import com.huacainfo.ace.partyschool.model.Files;
+import com.huacainfo.ace.partyschool.model.Student;
+import com.huacainfo.ace.partyschool.model.Teacher;
 
 
 public class FilesVo extends Files {
     private static final long serialVersionUID = 1L;
+    private Student student;
+
+    private Teacher teacher;
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
