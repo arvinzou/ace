1f66d2b95f3f46465d20e8cd6c37052817bed836
249134995@qq.com <249134995@qq.com>
2019-01-09 09:23:06 +0800

    市委党校班级文件-罗灿

--- ace/ace-partyschool/ace-partyschool-client/src/main/java/com/huacainfo/ace/partyschool/vo/FilesQVo.java
+++ ace/ace-partyschool/ace-partyschool-client/src/main/java/com/huacainfo/ace/partyschool/vo/FilesQVo.java
@@ -5,4 +5,14 @@ import com.huacainfo.ace.partyschool.model.Files;
 
 public class FilesQVo extends Files {
     private static final long serialVersionUID = 1L;
+
+    private  String mobile;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
