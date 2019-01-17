16dd8fec20d741af8c33dbf13271233ed3060497
249134995 <249134995@qq.com>
2019-01-17 16:44:21 +0800

    党校公告管理

--- ace/ace-partyschool/ace-partyschool-client/src/main/java/com/huacainfo/ace/partyschool/model/EvaluationRst.java
+++ ace/ace-partyschool/ace-partyschool-client/src/main/java/com/huacainfo/ace/partyschool/model/EvaluationRst.java
@@ -1,8 +1,10 @@
 package com.huacainfo.ace.partyschool.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class EvaluationRst {
+public class EvaluationRst  implements Serializable {
+    private  static final long serialVersionUID = 1L;
     private String id;
 
     private String evaluatingId;
