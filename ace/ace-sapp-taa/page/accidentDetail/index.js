64296ae2d4b16325d81ddf20bdeb746292419509
249134995@qq.com <249134995@qq.com>
2019-01-20 10:30:32 +0800

    交警事故分析-罗灿

--- ace/ace-sapp-taa/page/accidentDetail/index.js
+++ ace/ace-sapp-taa/page/accidentDetail/index.js
@@ -4,7 +4,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    tab: 0
+      items: [
+          { name: '1', value: '未按规定让行' },
+          { name: '2', value: '违反交通信号', checked: 'true' },
+          { name: '3', value: '酒后驾驶' },
+          { name: '4', value: '无证驾驶' },
+          { name: '5', value: '超速行驶' },
+          { name: '6', value: '违法倒车' },
+          { name: '7', value: '违法上道路行驶' },
+          { name: '8', value: '违法变道' },
+      ]
   },
 
   /**
@@ -13,18 +22,16 @@ Page({
   onLoad: function (options) {
     
   },
-  editAccident: function(){
-      wx.navigateTo({
-          url: '../accidentDetail/index',
-      });
-  },
+ 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
     
   },
-
+  checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  },
   /**
    * 生命周期函数--监听页面显示
    */
