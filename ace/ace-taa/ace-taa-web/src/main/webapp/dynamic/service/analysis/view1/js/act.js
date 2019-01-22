b604b0c6f19a35e11c9de1662755d67faf3c03e3
huacai003 <249134995@qq.com>
2019-01-22 15:35:07 +0800

    党校-为

--- ace/ace-taa/ace-taa-web/src/main/webapp/dynamic/service/analysis/view1/js/act.js
+++ ace/ace-taa/ace-taa-web/src/main/webapp/dynamic/service/analysis/view1/js/act.js
@@ -236,14 +236,14 @@ function initEvents() {
 	render($("#check-group-category"), data, "tpl-check-group");
 
 	$("input[name=startDate]").datetimepicker({
-		format: 'yyyy-mm-dd hh:ii:ss',
+        minView: "month",
+		format: 'yyyy-mm-dd',
 		language: 'zh-CN',
 		weekStart: 1,
 		todayBtn: 1, //显示‘今日’按钮
 		autoclose: 1,
 		todayHighlight: 1,
 		startView: 2,
-		minView: 'hour', //Number, String. 默认值：0, 'hour'，日期时间选择器所能够提供的最精确的时间选择视图。
 		clearBtn: true, //清除按钮
 		forceParse: 0
 	});
@@ -253,14 +253,14 @@ function initEvents() {
 
 
 	$("input[name=endDate]").datetimepicker({
-		format: 'yyyy-mm-dd hh:ii:ss',
+        minView: "month",
+		format: 'yyyy-mm-dd',
 		language: 'zh-CN',
 		weekStart: 1,
 		todayBtn: 1, //显示‘今日’按钮
 		autoclose: 1,
 		todayHighlight: 1,
 		startView: 2,
-		minView: 'hour', //Number, String. 默认值：0, 'hour'，日期时间选择器所能够提供的最精确的时间选择视图。
 		clearBtn: true, //清除按钮
 		forceParse: 0
 	});
