ba618ad4bb036c5226accc06a5e072b67defe6bd
huacai003 <249134995@qq.com>
2019-01-28 10:09:27 +0800

    党校-为

--- ace/ace-partyschool/ace-partyschool-web/src/main/webapp/www/registered/person/js/info.js
+++ ace/ace-partyschool/ace-partyschool-web/src/main/webapp/www/registered/person/js/info.js
@@ -148,6 +148,9 @@ function selectSex(obj,value){
 
 function isCardNo(card)
 {
+    if(!card){
+       return true;
+    }
 // 身份证号码为15位或者18位，15位时全为数字，18位前17位为数字，最后一位是校验位，可能为数字或字符X
     var reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
     if(reg.test(card) === false)
