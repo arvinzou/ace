27e8f33a06899bf0f3e1c5f06422d7c80ed40b05
249134995@qq.com <2011$aspire>
2019-02-26 10:39:14 +0800

    事故列表修改

--- ace/ace-sapp-taa/page/accidentList/index.js
+++ ace/ace-sapp-taa/page/accidentList/index.js
@@ -9,8 +9,8 @@ Page({
       list: [],
       rIndex: 0, //区域筛选索引
       region: [],
-      regionArray: [],  // 行政区下拉数据
-    
+      regionArray: [], // 行政区下拉数据
+
   },
 
   /**
@@ -21,10 +21,10 @@ Page({
     that.initAccidentList('');
       that.initRegionList();
   },
-  editAccident: function(){
-      wx.navigateTo({
-          url: '../accidentDetail/index',
-      });
+    editAccident: function () {
+        wx.navigateTo({
+            url: '../accidentDetail/index',
+        });
   },
     /**
      * 选择行政区
@@ -33,7 +33,7 @@ Page({
         var that = this;
         var tempIndex = e.detail.value;
         var areaArr = that.data.regionArray;
-        this.setData({
+        that.setData({
             rIndex: tempIndex,
             areaCode: areaArr[tempIndex].code
         });
@@ -68,109 +68,118 @@ Page({
         );
     },
 
-  initAccidentList: function(name){
-      var that = this;
-      util.request(cfg.server + '/taa/www/traAcc/findList', { start: 0, limit: 999, keyword:name},
-          function (res) {
-              if (res.status == 0) {
-                 that.setData({
-                     list: res.data.rows
-                 });
-              } else {
+    initAccidentList: function (name) {
+        var that = this;
+        util.request(cfg.server + '/taa/www/traAcc/findList', {
+                start: 0,
+                limit: 999,
+                keyword: name
+            },
+            function (res) {
+                if (res.status == 0) {
+                    that.setData({
+                        list: res.data.rows
+                    });
+                } else {
                     wx.showModal({
                         title: '提示',
                         content: res.info,
-                        success: function (res) { }
+                        success: function (res) {
+                        }
                     });
-              }
+                }
 
-          }
-      );
+            }
+        );
   },
     /**
      * 续报事件 */
-  editAccident: function(e){
-      var traAccId = e.target.dataset.id;
-      wx.navigateTo({
-          url: '../accidentDetail/index?id='+traAccId,
-      });
+    editAccident: function (e) {
+        var traAccId = e.target.dataset.id;
+        wx.navigateTo({
+            url: '../accidentDetail/index?id=' + traAccId,
+        });
   },
     /**
      * 撤销事件 */
-  revoke: function(e){
-      var traAccId = e.target.dataset.id;
-      var that = this;
-      util.request(cfg.server + '/taa/www/traAcc/repealReport', { traAccId: traAccId },
-          function (res) {
-              if (res.status == 0) {
-                  wx.showModal({
-                      title: '提示',
-                      content: res.info,
-                      success: function (res) { }
-                  });
-                  that.initAccidentList('');
-              } else {
-                  wx.showModal({
-                      title: '提示',
-                      content: res.info,
-                      success: function (res) { }
-                  });
-              }
+    revoke: function (e) {
+        var traAccId = e.target.dataset.id;
+        var that = this;
+        util.request(cfg.server + '/taa/www/traAcc/repealReport', {
+                traAccId: traAccId
+            },
+            function (res) {
+                if (res.status == 0) {
+                    wx.showModal({
+                        title: '提示',
+                        content: res.info,
+                        success: function (res) {
+                        }
+                    });
+                    that.initAccidentList('');
+                } else {
+                    wx.showModal({
+                        title: '提示',
+                        content: res.info,
+                        success: function (res) {
+                        }
+                    });
+                }
 
-          }
-      );
+            }
+        );
   },
-    searchRoad: function(e){
-      var that = this;
-      var name = e.detail.value;
-      that.initAccidentList(name);
+    searchRoad: function (e) {
+        var that = this;
+        var name = e.detail.value;
+        that.initAccidentList(name);
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
