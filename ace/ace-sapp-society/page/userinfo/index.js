cc06f8b291a12bc78719501ca913f64166fcdcaf
249134995@qq.com <2011$aspire>
2019-03-02 16:01:33 +0800

    芙蓉街道社会服务智慧平台：userinfo修改-黄龙

--- ace/ace-sapp-society/page/userinfo/index.js
+++ ace/ace-sapp-society/page/userinfo/index.js
@@ -32,10 +32,25 @@ Page({
         wx.navigateTo({ url: decodeURIComponent(that.data.url)});
     }
   },
+  /**
+   * 用户授权
+   */
   bindgetuserinfo: function (e) {
-    console.log(e.detail.userInfo);
     var that = this;
-    util.login(that.goback);
+    var userInfo = e.detail.userInfo;
+    console.log(e.detail.userInfo);
+    //判断用户是否授权
+    if (userInfo != null && userInfo != undefined){  //授权
+      util.login(that.goback);
+    } else {   //不授权
+      console.log(333, '执行到这里，说明拒绝了授权')
+      wx.showToast({
+        title: "为了您更好的体验,请先同意授权",
+        icon: 'none',
+        duration: 2000
+      });
+    }
+    
   },
 });
 
