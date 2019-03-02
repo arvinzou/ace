ca9559020492c43e6d4904a3071d4f58785a8c1f
249134995@qq.com <2011$aspire>
2019-03-02 15:55:12 +0800

    芙蓉街道社会服务智慧平台：util.js修改-黄龙

--- ace/ace-sapp-society/util/util.js
+++ ace/ace-sapp-society/util/util.js
@@ -1,111 +1,113 @@
 var cfg = require("../config.js");
+
 function formatLocation(longitude, latitude) {
-    if (typeof longitude === 'string' && typeof latitude === 'string') {
-        longitude = parseFloat(longitude)
-        latitude = parseFloat(latitude)
-    }
+  if (typeof longitude === 'string' && typeof latitude === 'string') {
+    longitude = parseFloat(longitude)
+    latitude = parseFloat(latitude)
+  }
 
-    longitude = longitude.toFixed(2)
-    latitude = latitude.toFixed(2)
+  longitude = longitude.toFixed(2)
+  latitude = latitude.toFixed(2)
 
-    return {
-        longitude: longitude.toString().split('.'),
-        latitude: latitude.toString().split('.')
-    }
+  return {
+    longitude: longitude.toString().split('.'),
+    latitude: latitude.toString().split('.')
+  }
 }
 
-
-
-
-
 function request(url, data, success, fail, complete) {
-    console.log('request url-->', url);
-    console.log('request data-->', data);
-    var _url = url,
-        _data = data,
-        _success = success,
-        _fail = fail,
-        _complete = complete;
-    wx.showNavigationBarLoading();
-    wx.showLoading({ title: "请求中" });
-    wx.request({
-        url: url,
-        data: data,
-        method: "GET",
-        dataType: "json",
-        header: {
-            'WX-SESSION-ID': wx.getStorageSync('WX-SESSION-ID') //每次请求带上登录标志
-        },
-        success: function (res) {
-            console.log(res);
-            wx.hideNavigationBarLoading() //完成停止加载
-            wx.hideLoading();
-            if (typeof _success == "function") {
-                _success(res.data);
-            }
-        },
-        fail: function (res) {
-            wx.hideNavigationBarLoading() //完成停止加载
-            wx.hideLoading();
-            if (typeof _fail == "function") {
-                _fail(res);
-            }
-        },
-        complete: function (res) {
-            if (typeof _complete == "function") {
-                _complete(res);
-            }
-        }
-    });
+  console.log('request url-->', url);
+  console.log('request data-->', data);
+  var _url = url,
+    _data = data,
+    _success = success,
+    _fail = fail,
+    _complete = complete;
+  wx.showNavigationBarLoading();
+  wx.showLoading({
+    title: "请求中"
+  });
+  wx.request({
+    url: url,
+    data: data,
+    method: "GET",
+    dataType: "json",
+    header: {
+      'WX-SESSION-ID': wx.getStorageSync('WX-SESSION-ID') //每次请求带上登录标志
+    },
+    success: function(res) {
+      console.log(res);
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.hideLoading();
+      if (typeof _success == "function") {
+        _success(res.data);
+      }
+    },
+    fail: function(res) {
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.hideLoading();
+      if (typeof _fail == "function") {
+        _fail(res);
+      }
+    },
+    complete: function(res) {
+      if (typeof _complete == "function") {
+        _complete(res);
+      }
+    }
+  });
 }
 
 function post(url, data, success, fail, complete) {
-    console.log('request url-->', url);
-    console.log('request data-->', data);
-    var _url = url,
-        _data = data,
-        _success = success,
-        _fail = fail,
-        _complete = complete;
-    wx.showNavigationBarLoading();
-    wx.showLoading({ title: "请求中" });
-    wx.request({
-        url: url,
-        data: data,
-        method: "post",
-        dataType: "json",
-        header: {
-            'WX-SESSION-ID': wx.getStorageSync('WX-SESSION-ID'),//每次请求带上登录标志
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        success: function (res) {
-            console.log(res);
-            wx.hideNavigationBarLoading() //完成停止加载
-            wx.hideLoading();
-            if (typeof _success == "function") {
-                _success(res.data);
-            }
-        },
-        fail: function (res) {
-            wx.hideNavigationBarLoading() //完成停止加载
-            wx.hideLoading();
-            if (typeof _fail == "function") {
-                _fail(res);
-            }
-        },
-        complete: function (res) {
-            if (typeof _complete == "function") {
-                _complete(res);
-            }
-        }
-    });
+  console.log('request url-->', url);
+  console.log('request data-->', data);
+  var _url = url,
+    _data = data,
+    _success = success,
+    _fail = fail,
+    _complete = complete;
+  wx.showNavigationBarLoading();
+  wx.showLoading({
+    title: "请求中"
+  });
+  wx.request({
+    url: url,
+    data: data,
+    method: "post",
+    dataType: "json",
+    header: {
+      'WX-SESSION-ID': wx.getStorageSync('WX-SESSION-ID'), //每次请求带上登录标志
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    success: function(res) {
+      console.log(res);
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.hideLoading();
+      if (typeof _success == "function") {
+        _success(res.data);
+      }
+    },
+    fail: function(res) {
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.hideLoading();
+      if (typeof _fail == "function") {
+        _fail(res);
+      }
+    },
+    complete: function(res) {
+      if (typeof _complete == "function") {
+        _complete(res);
+      }
+    }
+  });
 }
 
 function uuid() {
   var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-  var uuid = [], i;
-  var radix =chars.length;
-  var len=28;
+  var uuid = [],
+    i;
+  var radix = chars.length;
+  var len = 28;
   if (len) {
     for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
   } else {
@@ -121,10 +123,12 @@ function uuid() {
   }
   return uuid.join('');
 }
+
 function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
-} 
+}
+
 function formatTime(date, format) {
   var formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
   var returnArr = [];
@@ -142,22 +146,24 @@ function formatTime(date, format) {
   }
   return format;
 }
+
 function formatDate(date) {
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    return [year, month, day].map(formatNumber).join('-');
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  return [year, month, day].map(formatNumber).join('-');
 }
-function security(role){
+
+function security(role) {
   var userinfo = wx.getStorageSync('userinfo');
   console.log(userinfo);
-  if (!userinfo){
-      wx.showToast({
-        title: '身份未识别',
-        icon: 'none',
-        duration: 5000
-      })
-      return false;
+  if (!userinfo) {
+    wx.showToast({
+      title: '身份未识别',
+      icon: 'none',
+      duration: 5000
+    })
+    return false;
   }
   if (!userinfo.role) {
     wx.showToast({
@@ -168,7 +174,7 @@ function security(role){
     return false;
   }
   if (role) {
-    if (userinfo.role!=role) {
+    if (userinfo.role != role) {
       wx.showToast({
         title: '系统未授权',
         icon: 'none',
@@ -179,36 +185,41 @@ function security(role){
   }
   return true;
 }
+
 function isLogin() {
-    console.log("==============isLogin===============");
-    var userinfo = wx.getStorageSync('userinfo');
-    console.log(userinfo);
-    if (!userinfo.userProp) {
-        return false;
-    }
-    return true;
-} 
-
-
-function is_login(){
-    var userinfo = wx.getStorageSync('userinfo');
-    console.log(userinfo);
-    if(!userinfo){
-        return false;
-    }
-    return true;
+  console.log("==============isLogin===============");
+  var userinfo = wx.getStorageSync('userinfo');
+  console.log(userinfo);
+  if (!userinfo.userProp) {
+    return false;
+  }
+  return true;
 }
 
-function login(callback) {
-  var that = this;
+/**
+ * 是否授权
+ */
+function is_login() {
+  var userinfo = wx.getStorageSync('userinfo');
+  console.log(userinfo);
+  if (!userinfo) {
+    return false;
+  }
+  return true;
+}
+/**
+ * 获取地理位置
+ */
+function getLocation(callback) {
+  console.log('调用获取地理位置');
   var _callback = callback;
   wx.getLocation({
     type: 'gcj02',
-    success: function (rst) {
+    success: function(rst) {
       wx.login({
-        success: function (o) {
+        success: function(o) {
           wx.getUserInfo({
-            success: function (res) {
+            success: function(res) {
               wx.request({
                 url: cfg.loginUrl,
                 data: {
@@ -220,58 +231,83 @@ function login(callback) {
                   latitude: rst.latitude,
                   longitude: rst.longitude
                 },
-                success: function (res) {
+                success: function(res) {
                   wx.setStorageSync('WX-SESSION-ID', res.data.value['3rd_session']);
                   wx.setStorageSync('userinfo', res.data.value['userinfo']);
                   console.log('login success', res);
-                  if (_callback){
+                  if (_callback) {
                     console.log('login callback');
                     _callback(res);
                   }
                 },
-                fail: function ({ errMsg }) {
+                fail: function(errMsg) {
                   console.log('request fail', errMsg);
-                  wx.showModal({ title: "提示", showCancel: false, content: "鉴权失败" });
+                  wx.showModal({
+                    title: "提示",
+                    showCancel: false,
+                    content: "鉴权失败"
+                  });
                 }
               })
             }
           })
         }
       })
+    },
+    // 获取地理位置失败再次调用获取地理位置接口
+    fail: function(res) {
+      console.log(res);
+      wx.showModal({
+        title: '提示',
+        content: '请授权我们获取您的位置。',
+        complete:function(res) {
+          console.log(res);
+          if(res.confirm){
+            wx.openSetting({});
+          }
+        }
+      })
     }
   });
 }
+/**
+ * 登录获取信息
+ */
+function login(callback) {
+  var that = this;
+  getLocation(callback);
+}
 
 /** 
-  * 
-  * json转字符串 
-  */
+ * 
+ * json转字符串 
+ */
 function stringToJson(data) {
   return JSON.parse(data);
 }
 /** 
-*字符串转json 
-*/
+ *字符串转json 
+ */
 function jsonToString(data) {
   return JSON.stringify(data);
 }
 /** 
-*map转换为json 
-*/
+ *map转换为json 
+ */
 function mapToJson(map) {
   return JSON.stringify(strMapToObj(map));
 }
 /** 
-*json转换为map 
-*/
+ *json转换为map 
+ */
 function jsonToMap(jsonStr) {
   return objToStrMap(JSON.parse(jsonStr));
 }
 
 
 /** 
-*map转化为对象（map所有键都是字符串，可以将其转换为对象） 
-*/
+ *map转化为对象（map所有键都是字符串，可以将其转换为对象） 
+ */
 function strMapToObj(strMap) {
   let obj = Object.create(null);
   for (let [k, v] of strMap) {
@@ -281,8 +317,8 @@ function strMapToObj(strMap) {
 }
 
 /** 
-*对象转换为Map 
-*/
+ *对象转换为Map 
+ */
 function objToStrMap(obj) {
   let strMap = new Map();
   for (let k of Object.keys(obj)) {
@@ -290,14 +326,16 @@ function objToStrMap(obj) {
   }
   return strMap;
 }
-function extend(o1,o2){
+
+function extend(o1, o2) {
   let map = objToStrMap(o1);
   for (let k of Object.keys(o2)) {
     map.set(k, o2[k]);
   }
   return strMapToObj(map);
 }
-function initRadioGroupData(list,value){
+
+function initRadioGroupData(list, value) {
   for (var i = 0, len = list.length; i < len; ++i) {
     if (list[i].value == value) {
       list[i].checked = true;
@@ -317,69 +355,67 @@ function indexOf(arr, item) {
 }
 
 
-function setSysUser(sysUserInfo){
-    wx.setStorageSync('sysUserInfo', sysUserInfo);
+function setSysUser(sysUserInfo) {
+  wx.setStorageSync('sysUserInfo', sysUserInfo);
 }
 
-function getSysUser(){
-    var sysUserInfo = wx.getStorageSync('sysUserInfo');
-    if (!sysUserInfo){
-        return false;
-    }
-    return sysUserInfo;
+function getSysUser() {
+  var sysUserInfo = wx.getStorageSync('sysUserInfo');
+  if (!sysUserInfo) {
+    return false;
+  }
+  return sysUserInfo;
 }
 
-function delSysUser(){
-    wx.removeStorageSync('sysUserInfo');
+function delSysUser() {
+  wx.removeStorageSync('sysUserInfo');
 }
 
-function formateStringToDate(dateString){
-    dateString = dateString.replace(/-/g, "/");
-    return new Date(dateString);
+function formateStringToDate(dateString) {
+  dateString = dateString.replace(/-/g, "/");
+  return new Date(dateString);
 }
 
 
 module.exports = {
-    formatTime: formatTime,
-    formatLocation: formatLocation,
-    request: request,
-    post: post,
-    uuid: uuid,
-    formatDate:formatDate,
-    login:login,
-    isLogin: isLogin,
-    security: security,
-    stringToJson: stringToJson,
-    jsonToString: jsonToString,
-    mapToJson: mapToJson,
-    jsonToMap: jsonToMap,
-    strMapToObj: strMapToObj,
-    objToStrMap: objToStrMap,
-    extend: extend,
-    initRadioGroupData: initRadioGroupData,
-    indexOf: indexOf,
-    is_login: is_login,
-    getSysUser: getSysUser,
-    setSysUser: setSysUser,
-    delSysUser: delSysUser,
-    formateStringToDate: formateStringToDate
+  formatTime: formatTime,
+  formatLocation: formatLocation,
+  request: request,
+  post: post,
+  uuid: uuid,
+  formatDate: formatDate,
+  login: login,
+  isLogin: isLogin,
+  security: security,
+  stringToJson: stringToJson,
+  jsonToString: jsonToString,
+  mapToJson: mapToJson,
+  jsonToMap: jsonToMap,
+  strMapToObj: strMapToObj,
+  objToStrMap: objToStrMap,
+  extend: extend,
+  initRadioGroupData: initRadioGroupData,
+  indexOf: indexOf,
+  is_login: is_login,
+  getSysUser: getSysUser,
+  setSysUser: setSysUser,
+  delSysUser: delSysUser,
+  formateStringToDate: formateStringToDate
 }
 
 
-Date.prototype.Format = function (fmt) {
-    var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "H+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
+Date.prototype.Format = function(fmt) {
+  var o = {
+    "M+": this.getMonth() + 1, //月份 
+    "d+": this.getDate(), //日 
+    "H+": this.getHours(), //小时 
+    "m+": this.getMinutes(), //分 
+    "s+": this.getSeconds(), //秒 
+    "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+    "S": this.getMilliseconds() //毫秒 
+  };
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
 }
-
-
