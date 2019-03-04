8f6365577151fe40528040f304031676934de87e
249134995@qq.com <249134995@qq.com>
2019-03-04 14:10:00 +0800

    图书借阅

--- ace/ace-partyschool/ace-partyschool-web/src/main/webapp/www/registered/booksList/js/index.js
+++ ace/ace-partyschool/ace-partyschool-web/src/main/webapp/www/registered/booksList/js/index.js
@@ -1,20 +1,30 @@
-var server = 'http://192.168.2.100';
+
 $(function(){
     findList();
 });
 
 function findList(){
     $.ajax({
-        url: server+ "/api/www/api/findBorrowList",
+        url:  "/api/www/api/findBorrowList",
         type:"post",
         async:false,
-        contentType: "application/x-www-form-urlencoded; charset=utf-8",
         data:{
-            lCardNo: 'ZX112011'
+            lCardNo: 'Z0000012'
         },
         success:function(result){
             if(result.status == 0) {
-               console.log(result);
+                var list = result.data;
+                var unReturnList = [];
+                var resurnedList = [];
+                for(var i=0; i<list.length; i++){
+                    if(list[i].Signs == "未还"){
+                        unReturnList.push(list[i]);
+                    }else{
+                        resurnedList.push(list[i]);
+                    }
+                }
+                renderPage('unreturn', unReturnList, 'ulist-tpl');
+                renderPage('return', resurnedList, 'list-tpl');
             }else {
                 if(result.info){
                     alert(result.info);
@@ -28,4 +38,12 @@ function findList(){
             alert("系统服务内部异常！");
         }
     });
+}
+
+function renderPage(IDom, data, tempId) {
+    var tpl = document.getElementById(tempId).innerHTML;
+    var html = juicer(tpl, {
+        data: data,
+    });
+    $("#" + IDom).html(html);
 }
