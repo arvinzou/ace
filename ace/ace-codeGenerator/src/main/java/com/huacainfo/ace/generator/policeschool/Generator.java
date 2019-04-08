7fda137a0b8592e721ea6d89620e6a4342ac9a75
249134995@qq.com <249134995@qq.com>
2019-04-08 17:55:51 +0800

    commit by  :  ArvinZou module name:  codeGenerator change list: 1.新增自动代码生成器

---
+++ ace/ace-codeGenerator/src/main/java/com/huacainfo/ace/generator/policeschool/Generator.java
@@ -0,0 +1,26 @@
+package com.huacainfo.ace.generator.policeschool;
+
+import com.huacainfo.ace.generator.base.CodeGenerator;
+
+public class Generator {
+
+    public static void main(String[] args) throws Exception {
+        CodeGenerator codeGenerator = new CodeGenerator("policeschool", "群英考勤数据", "ArvinZou");
+        Class<?> c = com.huacainfo.ace.policeschool.model.QyAttRecord.class;
+
+        //*******************************************
+        codeGenerator.init(c);
+        //后端java代码
+        codeGenerator.generatorJavaManager(c);
+
+
+        //web形式-适用于新互联网架构
+//        codeGenerator.generatorWebClient1(c);
+
+
+        // web形式-适用于多表格，多数据的表单结构
+        codeGenerator.generatorWebClient2(c);
+
+    }
+
+}
