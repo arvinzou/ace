ebfffcf2a1c630f6023cffc12021da2fc5d205dd
249134995@qq.com <249134995@qq.com>
2019-04-09 09:26:19 +0800

    commit by  :  罗灿 module name:  glink change list:  智慧照明建筑物管理

---
+++ ace/ace-glink/ace-glink-service/src/main/java/com/huacainfo/ace/glink/dao/TopBuildingDao.java
@@ -0,0 +1,55 @@
+package com.huacainfo.ace.glink.dao;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.ibatis.annotations.Param;
+import com.huacainfo.ace.glink.model.TopBuilding;
+import com.huacainfo.ace.glink.vo.TopBuildingQVo;
+import com.huacainfo.ace.glink.vo.TopBuildingVo;
+
+public interface TopBuildingDao {
+
+TopBuilding selectByPrimaryKey(String id);
+
+int deleteByPrimaryKey(String id);
+
+int insert(TopBuilding record);
+
+
+int updateByPrimaryKey(TopBuilding record);
+
+
+TopBuildingVo selectVoByPrimaryKey(String id);
+
+List
+<TopBuildingVo> findList(@Param("condition") TopBuildingQVo condition,
+    @Param("start") int start,
+    @Param("limit") int limit,
+    @Param("orderBy") String orderBy);
+
+    int findCount(@Param("condition") TopBuildingQVo condition);
+
+    int isExit(TopBuilding record);
+
+    int updateStatus(@Param("id") String id,@Param("status") String status);
+
+
+    List
+    <Map
+    <String
+    ,Object>> getList(@Param("p")Map
+    <String
+    ,Object> p);
+
+
+    List
+    <Map
+    <String
+    , Object>> getListByCondition(@Param("params") Map
+    <String
+    , Object> params);
+
+
+    int deleteByPrimaryKeys(@Param("ids") String[] ids);
+
+    }
---
+++ ace/ace-glink/ace-glink-service/src/main/java/com/huacainfo/ace/glink/service/impl/TopBuildingServiceImpl.java
@@ -0,0 +1,355 @@
+package com.huacainfo.ace.glink.service.impl;
+
+
+import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import com.huacainfo.ace.common.tools.GUIDUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.huacainfo.ace.common.model.UserProp;
+import com.huacainfo.ace.common.result.MessageResponse;
+import com.huacainfo.ace.common.result.PageResult;
+import com.huacainfo.ace.common.result.SingleResult;
+import com.huacainfo.ace.common.tools.CommonUtils;
+import com.huacainfo.ace.glink.dao.TopBuildingDao;
+import com.huacainfo.ace.glink.model.TopBuilding;
+import com.huacainfo.ace.portal.service.DataBaseLogService;
+import com.huacainfo.ace.glink.service.TopBuildingService;
+import com.huacainfo.ace.glink.vo.TopBuildingVo;
+import com.huacainfo.ace.glink.vo.TopBuildingQVo;
+@Service("topBuildingService")
+/**
+* @author: luocan
+* @version: 2019-04-09
+* @Description:  TODO(建筑物管理)
+*/
+public class TopBuildingServiceImpl implements TopBuildingService {
+Logger logger = LoggerFactory.getLogger(this.getClass());
+@Autowired
+private TopBuildingDao topBuildingDao;
+@Autowired
+private DataBaseLogService dataBaseLogService;
+
+
+/**
+*
+* @Title:find!{bean.name}List
+* @Description:  TODO(建筑物管理分页查询)
+* @param:        @param condition
+* @param:        @param start
+* @param:        @param limit
+* @param:        @param orderBy
+* @param:        @throws Exception
+* @return:       PageResult
+<TopBuildingVo>
+    * @throws
+    * @author: luocan
+    * @version: 2019-04-09
+    */
+    @Override
+    public PageResult
+    <TopBuildingVo> findTopBuildingList(TopBuildingQVo condition, int start,int limit, String orderBy) throws
+        Exception {
+        PageResult
+        <TopBuildingVo> rst = new PageResult<>();
+            List
+            <TopBuildingVo> list = this.topBuildingDao.findList(condition,
+                start, limit, orderBy);
+                rst.setRows(list);
+                if (start <= 1) {
+                int allRows = this.topBuildingDao.findCount(condition);
+                rst.setTotal(allRows);
+                }
+                return rst;
+                }
+
+                /**
+                *
+                * @Title:insertTopBuilding
+                * @Description: TODO(添加建筑物管理)
+                * @param: @param o
+                * @param: @param userProp
+                * @param: @throws Exception
+                * @return: MessageResponse
+                * @throws
+                * @author: luocan
+                * @version: 2019-04-09
+                */
+                @Override
+                public MessageResponse insertTopBuilding(TopBuilding o, UserProp userProp) throws Exception {
+
+                if (CommonUtils.isBlank(o.getId())) {return new MessageResponse(1, "主键不能为空！");}if (CommonUtils.isBlank(o.getCode())) {return new MessageResponse(1, "建筑编号不能为空！");}if (CommonUtils.isBlank(o.getName())) {return new MessageResponse(1, "建筑名称不能为空！");}if (CommonUtils.isBlank(o.getType())) {return new MessageResponse(1, "建筑物类型不能为空！");}if (CommonUtils.isBlank(o.getAddress())) {return new MessageResponse(1, "所在地不能为空！");}if (CommonUtils.isBlank(o.getSubareaCode())) {return new MessageResponse(1, "分区编码不能为空！");}if (CommonUtils.isBlank(o.getStatus())) {return new MessageResponse(1, "状态不能为空！");}
+
+                int temp = this.topBuildingDao.isExit(o);
+                if (temp > 0) {
+                return new MessageResponse(1, "建筑物管理名称重复！");
+                }
+
+                o.setId(GUIDUtil.getGUID());
+                o.setCreateDate(new Date());
+                o.setStatus("1");
+                o.setCreateUserName(userProp.getName());
+                o.setCreateUserId(userProp.getUserId());
+                this.topBuildingDao.insert(o);
+                this.dataBaseLogService.log("添加建筑物管理", "建筑物管理", "",
+                o.getId(),o.getId(), userProp);
+
+                return new MessageResponse(0, "保存成功！");
+                }
+
+                /**
+                *
+                * @Title:updateTopBuilding
+                * @Description: TODO(更新建筑物管理)
+                * @param: @param o
+                * @param: @param userProp
+                * @param: @throws Exception
+                * @return: MessageResponse
+                * @throws
+                * @author: luocan
+                * @version: 2019-04-09
+                */
+                @Override
+                public MessageResponse updateTopBuilding(TopBuilding o, UserProp userProp) throws Exception {
+                if (CommonUtils.isBlank(o.getId())) {return new MessageResponse(1, "主键不能为空！");}if (CommonUtils.isBlank(o.getCode())) {return new MessageResponse(1, "建筑编号不能为空！");}if (CommonUtils.isBlank(o.getName())) {return new MessageResponse(1, "建筑名称不能为空！");}if (CommonUtils.isBlank(o.getType())) {return new MessageResponse(1, "建筑物类型不能为空！");}if (CommonUtils.isBlank(o.getAddress())) {return new MessageResponse(1, "所在地不能为空！");}if (CommonUtils.isBlank(o.getSubareaCode())) {return new MessageResponse(1, "分区编码不能为空！");}if (CommonUtils.isBlank(o.getStatus())) {return new MessageResponse(1, "状态不能为空！");}
+
+                o.setLastModifyDate(new Date());
+                o.setLastModifyUserName(userProp.getName());
+                o.setLastModifyUserId(userProp.getUserId());
+                this.topBuildingDao.updateByPrimaryKey(o);
+                this.dataBaseLogService.log("变更建筑物管理", "建筑物管理", "",
+                o.getId(), o.getId(), userProp);
+
+                return new MessageResponse(0, "保存成功！");
+                }
+
+                /**
+                *
+                * @Title:selectTopBuildingByPrimaryKey
+                * @Description: TODO(获取建筑物管理)
+                * @param: @param id
+                * @param: @throws Exception
+                * @return: SingleResult<TopBuilding>
+                * @throws
+                * @author: luocan
+                * @version: 2019-04-09
+                */
+                @Override
+                public SingleResult
+                <TopBuildingVo> selectTopBuildingByPrimaryKey(String id) throws Exception {
+                    SingleResult
+                    <TopBuildingVo> rst = new SingleResult<>();
+                        rst.setValue(this.topBuildingDao.selectVoByPrimaryKey(id));
+                        return rst;
+                        }
+
+                        /**
+                        *
+                        * @Title:deleteTopBuildingByTopBuildingId
+                        * @Description: TODO(删除建筑物管理)
+                        * @param: @param id
+                        * @param: @param userProp
+                        * @param: @throws Exception
+                        * @return: MessageResponse
+                        * @throws
+                        * @author: luocan
+                        * @version: 2019-04-09
+                        */
+                        @Override
+                        public MessageResponse deleteTopBuildingByTopBuildingId(String id, UserProp userProp) throws
+                        Exception {
+                        this.topBuildingDao.deleteByPrimaryKey(id);
+                        this.dataBaseLogService.log("删除建筑物管理", "建筑物管理", id, id,
+                        "建筑物管理", userProp);
+                        return new MessageResponse(0, "删除成功！");
+                        }
+
+
+                        /**
+                        *
+                        * @Title:audit
+                        * @Description: TODO(审核建筑物管理)
+                        * @param: @param id bean.id
+                        * @param: @param rst 审核结果 3-通过 4-拒绝
+                        * @param: @param remark 审核备注
+                        * @param: @throws Exception
+                        * @return: MessageResponse
+                        * @throws
+                        * @author: luocan
+                        * @version: 2019-04-09
+                        */
+                        @Override
+                        public MessageResponse audit(String id,String rst, String remark,
+                        UserProp userProp) throws Exception {
+
+
+                        dataBaseLogService.log("审核建筑物管理", "建筑物管理", id, id,
+                        "建筑物管理", userProp);
+                        return new MessageResponse(0, "审核成功！");
+                        }
+
+
+                        /**
+                        *
+                        * @Title:importXls
+                        * @Description: TODO(Excel导入资源数据)
+                        * @param: @param list
+                        * @param: @param userProp
+                        * @param: @return
+                        * @param: @throws Exception
+                        * @return: MessageResponse
+                        * @throws
+                        * @author: luocan
+                        * @version: 2019-04-09
+                        */
+
+                        @Override
+                        public MessageResponse importXls(List
+                        <Map
+                        <String
+                        , Object>> list, UserProp userProp) throws Exception {
+                        int i = 1;
+                        for (Map
+                        <String
+                        , Object> row : list) {
+                        TopBuilding o = new TopBuilding();
+                        CommonBeanUtils.copyMap2Bean(o,row);
+                        o.setCreateDate(new Date());
+                        o.setCreateUserId(userProp.getUserId());
+                        o.setCreateUserName(userProp.getName());
+                        o.setStatus("1");
+
+                        this.logger.info(o.toString());
+                        if (true) {
+                        return new MessageResponse(1,"行"+i+ ",编号不能为空！");
+                        }
+                        if (CommonUtils.isBlank(o.getId())) {return new MessageResponse(1, "主键不能为空！");}if (CommonUtils.isBlank(o.getCode())) {return new MessageResponse(1, "建筑编号不能为空！");}if (CommonUtils.isBlank(o.getName())) {return new MessageResponse(1, "建筑名称不能为空！");}if (CommonUtils.isBlank(o.getType())) {return new MessageResponse(1, "建筑物类型不能为空！");}if (CommonUtils.isBlank(o.getAddress())) {return new MessageResponse(1, "所在地不能为空！");}if (CommonUtils.isBlank(o.getSubareaCode())) {return new MessageResponse(1, "分区编码不能为空！");}if (CommonUtils.isBlank(o.getStatus())) {return new MessageResponse(1, "状态不能为空！");}
+                        int t = this.topBuildingDao.isExit(o);
+                        if (t > 0) {
+                        this.topBuildingDao.updateByPrimaryKey(o);
+
+                        } else {
+                        this.topBuildingDao.insert(o);
+                        }
+                        i++;
+                        }
+                        this.dataBaseLogService.log("建筑物管理导入", "建筑物管理", "",
+                        "rs.xls","rs.xls", userProp);
+                        return new MessageResponse(0, "导入成功！");
+                        }
+
+
+                        /**
+                        *
+                        * @Title:getList
+                        * @Description: TODO(条件查询数据)
+                        * @param: @param p
+                        * @param: @return
+                        * @param: @throws Exception
+                        * @return: ListResult
+                        <Map
+                        <String
+                        ,Object>>
+                        * @throws
+                        * @author: luocan
+                        * @version: 2019-04-09
+                        */
+                        @Override
+                        public ListResult
+                        <Map
+                        <String
+                        ,Object>> getList(Map
+                        <String
+                        , Object> p) throws Exception{
+                        ListResult
+                        <Map
+                        <String
+                        ,Object>> rst = new ListResult<>();
+                        rst.setValue(this.topBuildingDao.getList(p));
+
+                        return rst;
+
+                        }
+
+
+                        /**
+                        * @throws
+                        * @Title:getListByCondition
+                        * @Description: TODO(用于控件数据获取)
+                        * @param: @param params
+                        * @param: @return
+                        * @return: Map
+                        <String
+                        ,Object>
+                        * @author: luocan
+                        * @version: 2019-04-09
+                        */
+                        @Override
+                        public Map
+                        <String
+                        , Object> getListByCondition(Map
+                        <String
+                        , Object> params){
+                        Map
+                        <String
+                        , Object> rst = new HashMap
+                        <String
+                        , Object>();
+                        List
+                        <Map
+                        <String
+                        , Object>> list = this.topBuildingDao.getListByCondition(params);
+                        rst.put("total", list.size());
+                        rst.put("rows", list);
+                        return rst;
+                        }
+
+                        /**
+                        * @throws
+                        * @Title:deleteRoadSectionByRoadSectionIds
+                        * @Description: TODO(批量删除建筑物管理）
+                        * @param: @param ids
+                        * @param: @param userProp
+                        * @param: @throws Exception
+                        * @return: MessageResponse
+                        * @author: luocan
+                        * @version: 2019-04-09
+                        */
+                        @Override
+                        public MessageResponse deleteTopBuildingByTopBuildingIds(String [] id, UserProp userProp)
+                        throws Exception{
+
+                        this.topBuildingDao.deleteByPrimaryKeys(id);
+                        this.dataBaseLogService.log("批量删除建筑物管理", "建筑物管理", id[0],
+                        id[0], "建筑物管理", userProp);
+                        return new MessageResponse(0, "删除成功！");
+
+                        }
+
+
+                        /**
+                        * @throws
+                        * @Title:updateStatus
+                        * @Description: TODO(更新状态)
+                        * @param: @param obj
+                        * @param: @param userProp
+                        * @param: @throws Exception
+                        * @return: MessageResponse
+                        * @author: luocan
+                        * @version: 2019-04-09
+                        */
+                        @Override
+                        public MessageResponse updateStatus(String id,String status, UserProp userProp) throws
+                        Exception{
+                        this.topBuildingDao.updateStatus(id,status);
+                        this.dataBaseLogService.log("跟新状态", "建筑物管理", id, id,
+                        "建筑物管理", userProp);
+                        return new MessageResponse(0, "成功！");
+                        }
+
+                        }
