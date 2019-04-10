b7631ef80e5ea80dc4f426b268e0f643f253b214
249134995@qq.com <249134995@qq.com>
2019-04-10 20:24:36 +0800

    commit by  :  罗灿 module name:  glink change list:  智慧照明媒体文件下发

---
+++ ace/ace-glink/ace-glink-service/src/main/java/com/huacainfo/ace/glink/service/impl/AnimaLnkServiceImpl.java
@@ -0,0 +1,237 @@
+package com.huacainfo.ace.glink.service.impl;
+
+
+import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+import com.huacainfo.ace.common.tools.CommonBeanUtils;
+import com.huacainfo.ace.common.tools.GUIDUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.huacainfo.ace.common.model.UserProp;
+import com.huacainfo.ace.common.result.MessageResponse;
+import com.huacainfo.ace.common.result.PageResult;
+import com.huacainfo.ace.common.result.SingleResult;
+import com.huacainfo.ace.common.tools.CommonUtils;
+import com.huacainfo.ace.glink.dao.AnimaLnkDao;
+import com.huacainfo.ace.glink.model.AnimaLnk;
+import com.huacainfo.ace.portal.service.DataBaseLogService;
+import com.huacainfo.ace.glink.service.AnimaLnkService;
+import com.huacainfo.ace.glink.vo.AnimaLnkVo;
+import com.huacainfo.ace.glink.vo.AnimaLnkQVo;
+
+@Service("animaLnkService")
+/**
+ * @author: luocan
+ * @version: 2019-04-10
+ * @Description: TODO(节目上传)
+ */
+public class AnimaLnkServiceImpl implements AnimaLnkService {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private AnimaLnkDao animaLnkDao;
+    @Autowired
+    private DataBaseLogService dataBaseLogService;
+
+
+    /**
+     * @throws
+     * @Title:find!{bean.name}List
+     * @Description: TODO(节目上传分页查询)
+     * @param: @param condition
+     * @param: @param start
+     * @param: @param limit
+     * @param: @param orderBy
+     * @param: @throws Exception
+     * @return: PageResult
+     * <AnimaLnkVo>
+     * @author: luocan
+     * @version: 2019-04-10
+     */
+    @Override
+    public PageResult
+            <AnimaLnkVo> findAnimaLnkList(AnimaLnkQVo condition,
+                                          int start, int limit, String orderBy) throws Exception {
+        PageResult
+                <AnimaLnkVo> rst = new PageResult<>();
+        List
+                <AnimaLnkVo> list = this.animaLnkDao.findList(condition,
+                start, limit, orderBy);
+        rst.setRows(list);
+        if (start <= 1) {
+            int allRows = this.animaLnkDao.findCount(condition);
+            rst.setTotal(allRows);
+        }
+        return rst;
+    }
+
+    /**
+     * @throws
+     * @Title:insertAnimaLnk
+     * @Description: TODO(添加节目上传)
+     * @param: @param o
+     * @param: @param userProp
+     * @param: @throws Exception
+     * @return: MessageResponse
+     * @author: luocan
+     * @version: 2019-04-10
+     */
+    @Override
+    public MessageResponse insertAnimaLnk(AnimaLnk o, UserProp userProp) throws Exception {
+
+        if (CommonUtils.isBlank(o.getId())) {
+            return new MessageResponse(1, "主键不能为空！");
+        }
+        if (CommonUtils.isBlank(o.getAiCode())) {
+            return new MessageResponse(1, "节目编号不能为空！");
+        }
+        if (CommonUtils.isBlank(o.getLnkCode())) {
+            return new MessageResponse(1, "建筑/节点/站点编码不能为空！");
+        }
+
+
+        int temp = this.animaLnkDao.isExit(o);
+        if (temp > 0) {
+            return new MessageResponse(1, "节目上传名称重复！");
+        }
+
+        o.setId(GUIDUtil.getGUID());
+        o.setCreateDate(new Date());
+        o.setStatus("1");
+        this.animaLnkDao.insert(o);
+        this.dataBaseLogService.log("添加节目上传", "节目上传", "",
+                o.getId(), o.getId(), userProp);
+
+        return new MessageResponse(0, "保存成功！");
+    }
+
+    /**
+     * @throws
+     * @Title:updateAnimaLnk
+     * @Description: TODO(更新节目上传)
+     * @param: @param o
+     * @param: @param userProp
+     * @param: @throws Exception
+     * @return: MessageResponse
+     * @author: luocan
+     * @version: 2019-04-10
+     */
+    @Override
+    public MessageResponse updateAnimaLnk(AnimaLnk o, UserProp userProp) throws Exception {
+        if (CommonUtils.isBlank(o.getId())) {
+            return new MessageResponse(1, "主键不能为空！");
+        }
+        if (CommonUtils.isBlank(o.getAiCode())) {
+            return new MessageResponse(1, "节目编号不能为空！");
+        }
+        if (CommonUtils.isBlank(o.getLnkCode())) {
+            return new MessageResponse(1, "建筑/节点/站点编码不能为空！");
+        }
+        this.animaLnkDao.updateByPrimaryKey(o);
+        this.dataBaseLogService.log("变更节目上传", "节目上传", "",
+                o.getId(), o.getId(), userProp);
+
+        return new MessageResponse(0, "保存成功！");
+    }
+
+    /**
+     * @throws
+     * @Title:selectAnimaLnkByPrimaryKey
+     * @Description: TODO(获取节目上传)
+     * @param: @param id
+     * @param: @throws Exception
+     * @return: SingleResult<AnimaLnk>
+     * @author: luocan
+     * @version: 2019-04-10
+     */
+    @Override
+    public SingleResult
+            <AnimaLnkVo> selectAnimaLnkByPrimaryKey(String id) throws Exception {
+        SingleResult
+                <AnimaLnkVo> rst = new SingleResult<>();
+        rst.setValue(this.animaLnkDao.selectVoByPrimaryKey(id));
+        return rst;
+    }
+
+    /**
+     * @throws
+     * @Title:deleteAnimaLnkByAnimaLnkId
+     * @Description: TODO(删除节目上传)
+     * @param: @param id
+     * @param: @param userProp
+     * @param: @throws Exception
+     * @return: MessageResponse
+     * @author: luocan
+     * @version: 2019-04-10
+     */
+    @Override
+    public MessageResponse deleteAnimaLnkByAnimaLnkId(String id, UserProp userProp) throws
+            Exception {
+        this.animaLnkDao.deleteByPrimaryKey(id);
+        this.dataBaseLogService.log("删除节目上传", "节目上传", id, id,
+                "节目上传", userProp);
+        return new MessageResponse(0, "删除成功！");
+    }
+
+
+    /**
+     * @throws
+     * @Title:importXls
+     * @Description: TODO(Excel导入资源数据)
+     * @param: @param list
+     * @param: @param userProp
+     * @param: @return
+     * @param: @throws Exception
+     * @return: MessageResponse
+     * @author: luocan
+     * @version: 2019-04-10
+     */
+
+    @Override
+    public MessageResponse importXls(List
+                                             <Map
+                                                     <String
+                                                             , Object>> list, UserProp userProp) throws Exception {
+        int i = 1;
+        for (Map
+                <String
+                        , Object> row : list) {
+            AnimaLnk o = new AnimaLnk();
+            CommonBeanUtils.copyMap2Bean(o, row);
+            o.setCreateDate(new Date());
+            o.setStatus("1");
+
+            this.logger.info(o.toString());
+            if (true) {
+                return new MessageResponse(1, "行" + i + ",编号不能为空！");
+            }
+            if (CommonUtils.isBlank(o.getId())) {
+                return new MessageResponse(1, "主键不能为空！");
+            }
+            if (CommonUtils.isBlank(o.getAiCode())) {
+                return new MessageResponse(1, "节目编号不能为空！");
+            }
+            if (CommonUtils.isBlank(o.getLnkCode())) {
+                return new MessageResponse(1, "建筑/节点/站点编码不能为空！");
+            }
+
+            int t = this.animaLnkDao.isExit(o);
+            if (t > 0) {
+                this.animaLnkDao.updateByPrimaryKey(o);
+
+            } else {
+                this.animaLnkDao.insert(o);
+            }
+            i++;
+        }
+        this.dataBaseLogService.log("节目上传导入", "节目上传", "",
+                "rs.xls", "rs.xls", userProp);
+        return new MessageResponse(0, "导入成功！");
+    }
+
+}
