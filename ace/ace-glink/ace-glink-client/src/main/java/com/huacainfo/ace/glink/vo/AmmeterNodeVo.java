8a43a5629dd6041d27ceedde7e4d9f489640f785
249134995@qq.com <249134995@qq.com>
2019-04-15 17:26:36 +0800

    commit by  :  罗灿 module name:  glink change list:  智慧照明节点能耗信息

---
+++ ace/ace-glink/ace-glink-client/src/main/java/com/huacainfo/ace/glink/vo/AmmeterNodeVo.java
@@ -0,0 +1,20 @@
+package com.huacainfo.ace.glink.vo;
+
+import com.huacainfo.ace.glink.model.AmmeterNode;
+
+
+public class AmmeterNodeVo extends AmmeterNode {
+
+    /**
+     * 节点信息
+     */
+    private TopNodeVo topNodeVo;
+
+    public TopNodeVo getTopNodeVo() {
+        return topNodeVo;
+    }
+
+    public void setTopNodeVo(TopNodeVo topNodeVo) {
+        this.topNodeVo = topNodeVo;
+    }
+}
