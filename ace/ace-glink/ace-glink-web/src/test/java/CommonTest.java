46b875e0bdc84582e1a8105a0863390bfae512a5
249134995@qq.com <249134995@qq.com>
2019-04-30 14:21:46 +0800

    commit by  :  ArvinZou module name:  glink change list: 1、灯光策略下发接口调整

--- ace/ace-glink/ace-glink-web/src/test/java/CommonTest.java
+++ ace/ace-glink/ace-glink-web/src/test/java/CommonTest.java
@@ -1,5 +1,7 @@
 import com.huacainfo.ace.common.tools.GUIDUtil;
+import com.huacainfo.ace.common.tools.JsonUtil;
 import com.huacainfo.ace.glink.api.LeApiToolKit;
+import com.huacainfo.ace.glink.api.pojo.le.LightStrategyIn;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,9 +19,12 @@ public class CommonTest {
 
     @Test
     public void test() throws IOException {
+        String text = "{\"areaCode\":\"0000\",\"code\":\"22\",\"createDate\":\"20190430\",\"createUserId\":\"b7a29e42e42d4f61b1ed2342b6984c1d\",\"createUserName\":\"系统管理员\",\"id\":\"087c786b6d97496ca7bcc284c094b00c\",\"isWeek\":1,\"lastModifyDate\":\"20190430\",\"lastModifyUserId\":\"b7a29e42e42d4f61b1ed2342b6984c1d\",\"lastModifyUserName\":\"系统管理员\",\"months\":\"\",\"name\":\"22\",\"pattern\":\"1\",\"remark\":\"222\",\"startTime\":\"20190430\",\"status\":\"1\",\"stopTime\":\"20190510\",\"weeks\":\"1\",\"strategy\":\"SHGL#0003\",\"area\":\"22\"}";
+        LightStrategyIn in = JsonUtil.toObject(text, LightStrategyIn.class);
+        System.out.println(LeApiToolKit.lightStrategy(in));
 
-        //无数据返回
-        System.out.println(LeApiToolKit.getBuildingDetail());
+
+//        LeApiToolKit.lightStrategy();
     }
 
     private void generatorGUID(int num) {
@@ -28,7 +33,6 @@ public class CommonTest {
             System.out.println(GUIDUtil.getGUID());
         }
     }
-
 
 
 }
