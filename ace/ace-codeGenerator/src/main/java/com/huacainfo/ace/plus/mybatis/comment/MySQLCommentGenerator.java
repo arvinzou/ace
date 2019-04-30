e1227b108b42ba648c06526226b4633f08f18a44
249134995@qq.com <249134995@qq.com>
2019-04-30 15:49:48 +0800

    commit by  :  ArvinZou module name:  glink change list: 1、generator自定义注释生成器

--- ace/ace-codeGenerator/src/main/java/com/huacainfo/ace/plus/mybatis/comment/MySQLCommentGenerator.java
+++ ace/ace-codeGenerator/src/main/java/com/huacainfo/ace/plus/mybatis/comment/MySQLCommentGenerator.java
@@ -1,4 +1,4 @@
-package com.huacainfo.ace.api.mybatis.comment;
+package com.huacainfo.ace.plus.mybatis.comment;
 
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
@@ -47,17 +47,20 @@ public class MySQLCommentGenerator extends EmptyCommentGenerator {
 
     @Override
     public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        String author = properties.getProperty("author");
+
         String dateFormat = properties.getProperty("dateFormat", "yyyy-MM-dd");
         SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat);
 
         // 获取表注释
+        String tableName = introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime();
         String remarks = introspectedTable.getRemarks();
+        Properties prop = System.getProperties();
 
         topLevelClass.addJavaDocLine("/**");
-        topLevelClass.addJavaDocLine(" * " + remarks);
+        topLevelClass.addJavaDocLine(" * 表名：" + tableName);
+        topLevelClass.addJavaDocLine(" * 注释：" + remarks);
         topLevelClass.addJavaDocLine(" *");
-        topLevelClass.addJavaDocLine(" * @author " + author);
+        topLevelClass.addJavaDocLine(" * @author " + prop.getProperty("user.name"));
         topLevelClass.addJavaDocLine(" * @date " + dateFormatter.format(new Date()));
         topLevelClass.addJavaDocLine(" */");
     }
@@ -65,9 +68,13 @@ public class MySQLCommentGenerator extends EmptyCommentGenerator {
     @Override
     public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
         // 获取列注释
+        String tableName = introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime();
+        String columnName = introspectedColumn.getActualColumnName();
         String remarks = introspectedColumn.getRemarks();
+
         field.addJavaDocLine("/**");
-        field.addJavaDocLine(" * " + remarks);
+        field.addJavaDocLine(" * 字段名：" + tableName + "." + columnName);
+        field.addJavaDocLine(" * 注释：" + remarks);
         field.addJavaDocLine(" */");
     }
 }
